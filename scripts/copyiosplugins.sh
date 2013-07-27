9ff26cddff5a72d01eb414983db93ed821e77a6b
Atilim Cetin <atilim.cetin@gmail.com>
2013-07-27 13:34:51 +0300

    Modified for mercurial.

--- scripts/copyiosplugins.sh
+++ scripts/copyiosplugins.sh
@@ -1,3 +1,7 @@
+mkdir ~/myprojects/gideros/ios/GiderosiOSPlayer/GiderosiOSPlayer/Plugins
+mkdir ~/myprojects/gideros/ui/Templates/Xcode4/iOS\ Template/iOS\ Template/Plugins
+mkdir /Volumes/gideros/ios/GiderosiOSPlayer/GiderosiOSPlayer/Plugins
+mkdir /Volumes/gideros/ui/Templates/Xcode4/iOS\ Template/iOS\ Template/Plugins
 cp ../plugins/LuaSocket/source/libluasocket.a ~/myprojects/gideros/ios/GiderosiOSPlayer/GiderosiOSPlayer/Plugins
 cp ../plugins/LuaSocket/source/libluasocket.a ~/myprojects/gideros/ui/Templates/Xcode4/iOS\ Template/iOS\ Template/Plugins
 cp ../plugins/LuaSocket/source/libluasocket.a /Volumes/gideros/ios/GiderosiOSPlayer/GiderosiOSPlayer/Plugins
