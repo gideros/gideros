2f6b8dc5414e8306234d745e2a3343df2754a83b
Atilim Cetin <atilim.cetin@gmail.com>
2013-09-07 23:56:21 +0300

    Organized makefiles of Facebook Android plugin.

--- plugins/Facebook/source/Android/jni/Android.mk
+++ plugins/Facebook/source/Android/jni/Android.mk
@@ -6,27 +6,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE            := gideros
-LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgideros.so
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-#
-# Lua Socket Library
-# 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE            := luasocket
-LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libluasocket.so
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-#
-# Lua File System Library
-# 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE            := lfs
-LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/liblfs.so
+LOCAL_SRC_FILES         := ../../../../../Sdk/lib/android/$(TARGET_ARCH_ABI)/libgideros.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -36,8 +16,8 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE           := facebook
-LOCAL_ARM_MODE         := arm
 LOCAL_CFLAGS           := -O2
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../../../../Sdk/include $(LOCAL_PATH)/..
 LOCAL_SRC_FILES        := facebookbinder.cpp gfacebook.cpp
 LOCAL_LDLIBS           := -ldl -llog
 LOCAL_SHARED_LIBRARIES := gideros
--- plugins/Facebook/source/Android/jni/Application.mk
+++ plugins/Facebook/source/Android/jni/Application.mk
@@ -3,5 +3,3 @@ APP_CPPFLAGS += -fexceptions -frtti
 APP_PLATFORM := android-8
 APP_ABI := armeabi armeabi-v7a
 APP_OPTIM := release
-
-
