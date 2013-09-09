1b514fb1947596587c8828ace13ad35bc2ff9e4e
Atilim Cetin <atilim.cetin@gmail.com>
2013-09-09 23:43:46 +0300

    Initial commit of new Flurry plugin.

---
+++ plugins/Flurry/source/iOS/flurrybinder.cpp
@@ -0,0 +1,128 @@
+#include "gflurry.h"
+#include "gideros.h"
+#include <vector>
+#include <string>
+
+#ifndef abs_index
+#define abs_index(L, i) ((i) > 0 || (i) <= LUA_REGISTRYINDEX ? (i) : lua_gettop(L) + (i) + 1)
+#endif
+
+static int isSessionStarted = 0;
+
+static int isAvailable(lua_State *L)
+{
+	lua_pushboolean(L, 1);
+	return 1;
+}
+
+static int startSession(lua_State *L)
+{
+	if (!isSessionStarted)
+		return 0;
+    
+    gflurry_StartSession(luaL_checkstring(L, 1));
+
+	isSessionStarted = 1;
+    
+	return 0;
+}
+
+static char **copyParameters(lua_State *L, int index)
+{
+	int t = abs_index(L, index);
+    
+    std::vector<std::string> parameters;
+	lua_pushnil(L);
+	while (lua_next(L, t) != 0)
+	{
+        parameters.push_back(luaL_checkstring(L, -2));
+        parameters.push_back(luaL_checkstring(L, -1));
+		lua_pop(L, 1);
+    }
+    
+    char **parameters2 = (char**)malloc(parameters.size() * sizeof(char*));
+    for (std::size_t i = 0; i < parameters.size(); ++i)
+        parameters2[i] = strdup(parameters[i].c_str());
+
+    return parameters2;
+}
+
+static void freeParameters(char **parameters)
+{
+    if (parameters == NULL)
+        return;
+    
+    while (*parameters)
+        free(*parameters++);
+
+    free(parameters);
+}
+
+static int logEvent(lua_State *L)
+{
+    const char *eventName = luaL_checkstring(L, 1);
+    
+    char **parameters = NULL;
+    if (!lua_isnoneornil(L, 2))
+        parameters = copyParameters(L, 2);
+    
+    int timed = lua_toboolean(L, 3);
+    
+    gflurry_LogEvent(eventName, (const char **)parameters, timed);
+    
+    freeParameters(parameters);
+    
+    return 0;
+}
+
+static int endTimedEvent(lua_State *L)
+{
+    const char *eventName = luaL_checkstring(L, 1);
+    
+    char **parameters = NULL;
+    if (!lua_isnoneornil(L, 2))
+        parameters = copyParameters(L, 2);
+
+    gflurry_EndTimedEvent(eventName, (const char **)parameters);
+    
+    freeParameters(parameters);
+    
+    return 0;
+}
+
+static int loader(lua_State* L)
+{
+	lua_newtable(L);
+    
+	lua_pushcfunction(L, isAvailable);
+	lua_setfield(L, -2, "isAvailable");
+	lua_pushcfunction(L, startSession);
+	lua_setfield(L, -2, "startSession");
+	lua_pushcfunction(L, logEvent);
+	lua_setfield(L, -2, "logEvent");
+	lua_pushcfunction(L, endTimedEvent);
+	lua_setfield(L, -2, "endTimedEvent");
+    
+	lua_pushvalue(L, -1);
+	lua_setglobal(L, "flurry");
+    
+	return 1;
+}
+
+static void g_initializePlugin(lua_State *L)
+{
+	lua_getglobal(L, "package");
+	lua_getfield(L, -1, "preload");
+    
+	lua_pushcfunction(L, loader);
+	lua_setfield(L, -2, "flurry");
+    
+	lua_pop(L, 2);
+}
+
+static void g_deinitializePlugin(lua_State *L)
+{
+}
+
+REGISTER_PLUGIN("Flurry", "1.0")
+
---
+++ plugins/Flurry/source/iOS/gflurry-ios.m
@@ -0,0 +1,57 @@
+#include "gflurry.h"
+#import "Flurry.h"
+
+static NSDictionary *getDictionary(const char **parameters)
+{
+    if (parameters == NULL)
+        return nil;
+    
+    NSMutableDictionary *parameters2 = [NSMutableDictionary dictionaryWithCapacity:5];
+    while (true)
+    {
+        if (*parameters == NULL)
+            break;
+        NSString *key = [NSString stringWithUTF8String:*parameters++];
+        
+        if (*parameters == NULL)
+            break;
+        NSString *value = [NSString stringWithUTF8String:*parameters++];
+        
+        [parameters2 setObject:value forKey:key];
+    }
+
+    return parameters2;
+}
+
+void gflurry_StartSession(const char *apiKey)
+{
+    [Flurry startSession:[NSString stringWithUTF8String:apiKey]];
+}
+
+void gflurry_LogEvent(const char *eventName, const char **parameters, int timed)
+{
+    NSDictionary *parameters2 = getDictionary(parameters);
+    
+    NSString *eventName2 = [NSString stringWithUTF8String:eventName];
+    
+    if (parameters2 && timed)
+        [Flurry logEvent:eventName2 withParameters:parameters2 timed:YES];
+    else if (!parameters2 && timed)
+        [Flurry logEvent:eventName2 timed:YES];
+    else if (parameters2 && !timed)
+        [Flurry logEvent:eventName2 withParameters:parameters2];
+    else if (!parameters2 && !timed)
+        [Flurry logEvent:eventName2];
+    
+}
+
+void gflurry_EndTimedEvent(const char *eventName, const char **parameters)
+{
+    NSDictionary *parameters2 = getDictionary(parameters);
+    if (parameters2 == nil)
+        parameters2 = [NSDictionary dictionary];
+    
+    NSString *eventName2 = [NSString stringWithUTF8String:eventName];
+    
+    [Flurry endTimedEvent:eventName2 withParameters:parameters2];
+}
