de1ebc7b1be3e30a211c1d789b365c08c2dfa4bd
Atilim Cetin <atilim.cetin@gmail.com>
2013-09-24 12:52:31 +0300

    Updated Xcode project files (part 4).

--- scripts/buildiosplugins.sh
+++ scripts/buildiosplugins.sh
@@ -3,8 +3,8 @@ cd plugins
 cd LuaSocket
 cd source
 rm -rf build
-xcodebuild -project luasocket.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
-xcodebuild -project luasocket.xcodeproj -alltargets -sdk iphoneos5.1 -configuration Release
+xcodebuild -project luasocket.xcodeproj -alltargets -sdk iphonesimulator6.1 -configuration Release
+xcodebuild -project luasocket.xcodeproj -alltargets -sdk iphoneos6.1 -configuration Release
 lipo build/Release-iphoneos/libluasocket.a build/Release-iphonesimulator/libluasocket.a -create -output libluasocket.a
 cd ../../..
 cd scripts
