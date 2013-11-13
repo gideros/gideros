4c2964e938a29103ab552768d576c7eb3492c89a
Atilim Cetin <atilim.cetin@gmail.com>
2013-11-13 15:45:43 +0200

    Added lua.xcodeproj (part 1).

---
+++ lua/lua/lua.m
@@ -0,0 +1,13 @@
+//
+//  lua.m
+//  lua
+//
+//  Created by Atilim Cetin on 13/11/13.
+//  Copyright (c) 2013 Gideros Mobile. All rights reserved.
+//
+
+#import "lua.h"
+
+@implementation lua
+
+@end
