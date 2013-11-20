73e0df44c75ba8d32e41e3f26f3379070055eede
Atilim Cetin <atilim.cetin@gmail.com>
2013-11-20 09:32:06 +0200

    [minor]

--- player/copydlls.bat
+++ player/copydlls.bat
@@ -1,12 +1,22 @@
-copy ..\libfoundation\release\foundation.dll release
-copy ..\libfoundation\release\foundation.dll debug
-copy ..\libgid\release\gid.dll release
-copy ..\libgid\release\gid.dll debug
-copy ..\lua\release\lua.dll release
-copy ..\lua\release\lua.dll debug
-copy ..\libgideros\release\gideros.dll release
-copy ..\libgideros\release\gideros.dll debug
-copy ..\libgfile\release\gfile.dll release
-copy ..\libgfile\release\gfile.dll debug
+cp ..\libgid\release\gid.dll release
+cp ..\libgvfs\release\gvfs.dll release
+cp ..\lua\release\lua.dll release
+cp ..\libgideros\release\gideros.dll release
+cp ..\libpystring\release\pystring.dll release
+cp ..\libgid\external\zlib-1.2.8\build\mingw\zlib.dll release
+cp ..\libgid\external\glew-1.10.0\lib\mingw\glew32.dll release
+cp ..\libgid\external\openal-soft-1.13\build\mingw\OpenAL32.dll release
+cp ..\libgid\external\pthreads-w32-2-8-0-release\Pre-built.2\lib\pthreadGC2.dll release
 
+cp ..\libgid\release\gid.dll debug
+cp ..\libgvfs\release\gvfs.dll debug
+cp ..\lua\release\lua.dll debug
+cp ..\libgideros\release\gideros.dll debug
+cp ..\libpystring\release\pystring.dll debug
+cp ..\libgid\external\zlib-1.2.8\build\mingw\zlib.dll debug
+cp ..\libgid\external\glew-1.10.0\lib\mingw\glew32.dll debug
+cp ..\libgid\external\openal-soft-1.13\build\mingw\OpenAL32.dll debug
+cp ..\libgid\external\pthreads-w32-2-8-0-release\Pre-built.2\lib\pthreadGC2.dll debug
 
+cp ..\luajit\src\lua51.dll release\lua.dll
+cp ..\luajit\src\lua51.dll debug\lua.dll
