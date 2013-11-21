3291cc5b174dc12653bfe1a2a29ed2de69655441
Atilim Cetin <atilim.cetin@gmail.com>
2013-11-21 15:23:00 +0200

    Organized build script for Qt in Mac.

--- scripts/buildqtlibs.sh
+++ scripts/buildqtlibs.sh
@@ -1,6 +1,6 @@
 sudo rm /usr/lib/libpystring.1.dylib
+sudo rm /usr/lib/libgvfs.1.dylib
 sudo rm /usr/lib/libgid.1.dylib
-sudo rm /usr/lib/libgfile.1.dylib
 sudo rm /usr/lib/liblua.1.dylib
 sudo rm /usr/lib/libgideros.1.dylib
 
@@ -14,19 +14,18 @@ make
 sudo cp libpystring.1.dylib /usr/lib
 cd ..
 
+cd libgvfs
+/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake libgvfs.pro
+make clean
+make
+sudo cp libgvfs.1.dylib /usr/lib
+cd ..
+
 cd libgid
 /Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake libgid.pro
 make clean
 make
 sudo cp libgid.1.dylib /usr/lib
-cd ..
-
-
-cd libgfile
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake libgfile.pro
-make clean
-make
-sudo cp libgfile.1.dylib /usr/lib
 cd ..
 
 cd lua
@@ -52,13 +51,13 @@ cp ../libgideros/gplugin.h include
 cp ../libgideros/gproxy.h include
 cp ../libgideros/greferenced.h include
 cp ../libgideros/gexport.h include
-cp ../libgfile/gfile.h include
+cp ../libgvfs/gfile.h include
 cp ../lua/src/lua.h include
 cp ../lua/src/luaconf.h include
 cp ../lua/src/lualib.h include
 cp ../lua/src/lauxlib.h include
 cp ../libgid/include/gglobal.h include
-cp ../libgid/include/gpath.h include
+cp ../libgvfs/gpath.h include
 cp ../libgid/include/glog.h include
 cp ../libgid/include/gapplication.h include
 cp ../libgid/include/gevent.h include
@@ -66,13 +65,13 @@ cp ../libgid/include/gevent.h include
 
 mkdir lib
 mkdir lib/desktop
-cp ../libgfile/libgfile.1.dylib lib/desktop
+cp ../libgvfs/libgvfs.1.dylib lib/desktop
 cp ../libgideros/libgideros.1.dylib lib/desktop
 cp ../lua/liblua.1.dylib lib/desktop
 cp ../libgid/libgid.1.dylib lib/desktop
 
 cd lib/desktop
-ln -s libgfile.1.dylib libgfile.dylib
+ln -s libgvfs.1.dylib libgvfs.dylib
 ln -s libgideros.1.dylib libgideros.dylib
 ln -s liblua.1.dylib liblua.dylib
 ln -s libgid.1.dylib libgid.dylib
