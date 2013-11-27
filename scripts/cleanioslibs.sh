052698d7e318275b70214a904dcb0d1139d2abd0
Atilim Cetin <atilim.cetin@gmail.com>
2013-11-27 23:55:29 +0200

    Added zlib to iOS projects.

--- scripts/cleanioslibs.sh
+++ scripts/cleanioslibs.sh
@@ -1,15 +1,27 @@
 cd ..
 
+cd libgid/external/zlib-1.2.8
+rm -rf build/Release-iphoneos
+rm -rf build/Release-iphonesimulator
+rm -rf build/zlib.build
+cd ../../..
+
 cd libgvfs
-rm -rf build
+rm -rf build/Release-iphoneos
+rm -rf build/Release-iphonesimulator
+rm -rf build/gvfs.build
 cd ..
 
 cd lua
-rm -rf build
+rm -rf build/Release-iphoneos
+rm -rf build/Release-iphonesimulator
+rm -rf build/lua.build
 cd ..
 
 cd ios/iosplayer
-rm -rf build
+rm -rf build/Release-iphoneos
+rm -rf build/Release-iphonesimulator
+rm -rf build/iosplayer.build
 cd ../..
 
 cd scripts
