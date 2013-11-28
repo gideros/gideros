271410cec8865a4b73a7eda8c57a9353b039d1ec
Atilim Cetin <atilim.cetin@gmail.com>
2013-11-28 11:47:30 +0200

    [minor]

--- scripts/buildandroidplugins.bat
+++ scripts/buildandroidplugins.bat
@@ -4,65 +4,65 @@ cd plugins
 cd BitOp
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd LuaSocket
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd LPeg
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd LuaFileSystem
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd "Google Billing"
 cd source
 cd Android
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..\..
 
 cd LuaSQLite3
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd Microphone
 cd source
 cd Android
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..\..
 
 cd Flurry
 cd source
 cd Android
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..\..
 
 cd Facebook
 cd source
 cd Android
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..\..
 
 cd JSON
 cd source
 rm -rf libs obj
-call c:\android-ndk-r9\ndk-build
+call c:\android-ndk-r9b\ndk-build
 cd ..\..
 
 cd ..
