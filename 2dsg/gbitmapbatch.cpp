810d10d0dcfcec732eb0a9c2807b8c16283210cf
Atilim Cetin <atilim.cetin@gmail.com>
2013-12-19 15:12:54 +0200

    Uncomplete BitmapArray class.

--- 2dsg/gbitmapbatch.cpp
+++ 2dsg/gbitmapbatch.cpp
@@ -1,56 +1,64 @@
-#include "gspritebatch.h"
+#include "gbitmapbatch.h"
+#include "bitmapdata.h"
 
-GSpriteBatch::GSpriteBatch(Application *application) : Sprite(application)
+GBitmapArray::GBitmapArray(Application *application, BitmapData *bitmapdata) : Sprite(application)
 {
-    sprite_ = NULL;
+    bitmapdata_ = bitmapdata;
+    bitmapdata_->ref();
+
+    texturebase_ = NULL;
+
+    anchorx_ = 0;
+    anchory_ = 0;
 }
 
-GSpriteBatch::~GSpriteBatch()
+GBitmapArray::GBitmapArray(Application *application, TextureBase *texturebase) : Sprite(application)
 {
-    if (sprite_)
-        sprite_->unref();
+    texturebase_ = texturebase;
+    texturebase_->ref();
+
+    bitmapdata_ = NULL;
+
+    anchorx_ = 0;
+    anchory_ = 0;
 }
 
-void GSpriteBatch::setSprite(Sprite *sprite)
-{
-    if (sprite)
-        sprite->ref();
-    if (sprite_)
-        sprite_->unref();
-    sprite_ = sprite;
-}
-
-void GSpriteBatch::setPosition(int i, float x, float y)
-{
-
-}
-
-void GSpriteBatch::setRotation(int i, float rotation)
+GBitmapArray::~GBitmapArray()
 {
 
 }
 
-void GSpriteBatch::setScale(int i, float x, float y)
+void GBitmapArray::setPosition(int i, float x, float y)
 {
 
 }
 
-void GSpriteBatch::setColorTransform(int i, float r, float g, float b, float a)
+void GBitmapArray::setRotation(int i, float rotation)
 {
 
 }
 
-void GSpriteBatch::setAlpha(int i, float a)
+void GBitmapArray::setScale(int i, float x, float y)
 {
 
 }
 
-void GSpriteBatch::doDraw(const CurrentTransform&, float sx, float sy, float ex, float ey)
+void GBitmapArray::setColorTransform(int i, float r, float g, float b, float a)
 {
 
 }
 
-void GSpriteBatch::extraBounds(float* minx, float* miny, float* maxx, float* maxy) const
+void GBitmapArray::setAlpha(int i, float a)
+{
+
+}
+
+void GBitmapArray::doDraw(const CurrentTransform&, float sx, float sy, float ex, float ey)
+{
+
+}
+
+void GBitmapArray::extraBounds(float* minx, float* miny, float* maxx, float* maxy) const
 {
 
 }
--- 2dsg/gbitmapbatch.h
+++ 2dsg/gbitmapbatch.h
@@ -4,19 +4,38 @@
 #include "sprite.h"
 
 class Application;
+class BitmapData;
+class TextureBase;
 
-class GSpriteBatch : public Sprite
+class GBitmapArray : public Sprite
 {
 public:
-    GSpriteBatch(Application *application);
-    virtual ~GSpriteBatch();
+    GBitmapArray(Application *application, BitmapData *bitmapdata);
+    GBitmapArray(Application *application, TextureBase *texturebase);
+    virtual ~GBitmapArray();
 
-    void setSprite(Sprite *sprite);
+    void setTextureRegion(BitmapData *bitmapdata);
+    void setTexture(TextureBase *texturebase);
+
+    void setAnchorPoint(float x, float y);
+    void getAnchorPoint(float *x, float *y) const;
+
     void setPosition(int i, float x, float y);
     void setRotation(int i, float rotation);
     void setScale(int i, float x, float y);
     void setColorTransform(int i, float r, float g, float b, float a);
     void setAlpha(int i, float a);
+    void setVisible(int i, bool visible);
+
+    void getPosition(int i, float *x, float *y) const;
+    float getRotation(int i) const;
+    void getScale(int i, float *x, float *y) const;
+    void getColorTransform(int i, float *r, float *g, float *b, float *a) const;
+    float getAlpha(int i) const;
+    const void getVisible(int i) const;
+
+    size_t getSize() const;
+    void setSize(size_t size);
 
 private:
     virtual void doDraw(const CurrentTransform&, float sx, float sy, float ex, float ey);
@@ -25,7 +44,10 @@ private:
     virtual void extraBounds(float* minx, float* miny, float* maxx, float* maxy) const;
 
 private:
-    Sprite *sprite_;
+    BitmapData  *bitmapdata_;
+    TextureBase *texturebase_;
+
+    float anchorx_, anchory_;
 };
 
 #endif
