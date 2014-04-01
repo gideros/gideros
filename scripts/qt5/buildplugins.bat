997e86473a9421b6f660700bfe0478adf8035a88
Atilim Cetin <atilim.cetin@gmail.com>
2014-04-01 16:07:59 +0300

    Arranging build scripts.

---
+++ scripts/qt5/buildplugins.bat
@@ -0,0 +1,66 @@
+pushd \
+call C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\qtenv2.bat
+popd
+
+cd ..
+cd plugins
+
+cd BitOp
+cd source
+qmake bitop.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd LuaSQLite3
+cd source
+qmake lsqlite3.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd LuaSocket
+cd source
+qmake luasocket.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd LPeg
+cd source
+qmake lpeg.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd LuaFileSystem
+cd source
+qmake lfs.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd Microphone
+cd source
+cd Desktop
+qmake microphone.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..\..
+
+cd JSON
+cd source
+qmake json.pro
+mingw32-make.exe clean
+mingw32-make.exe release
+mingw32-make.exe release
+cd ..\..
+
+cd ..
+cd scripts
