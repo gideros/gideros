80111ee5dce6607498722d9e09ab25e4119062ce
Atilim Cetin <atilim.cetin@gmail.com>
2014-04-01 16:20:34 +0300

    Arranging build scripts.

---
+++ scripts/qt5/cleanqt.bat
@@ -0,0 +1,55 @@
+pushd \
+call C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\qtenv2.bat
+popd
+
+cd ..
+
+cd texturepacker
+mingw32-make.exe clean
+qmake texturepacker.pro
+mingw32-make.exe clean
+cd ..
+
+cd fontcreator
+mingw32-make.exe clean
+qmake fontcreator.pro
+mingw32-make.exe clean
+cd ..
+
+cd ui
+mingw32-make.exe clean
+qmake ui.pro
+mingw32-make.exe clean
+cd ..
+
+cd player
+mingw32-make.exe clean
+qmake player.pro
+mingw32-make.exe clean
+cd ..
+
+cd licensemanager
+mingw32-make.exe clean
+qmake licensemanager.pro
+mingw32-make.exe clean
+cd ..
+
+cd gdrdeamon
+mingw32-make.exe clean
+qmake gdrdeamon.pro
+mingw32-make.exe clean
+cd ..
+
+cd gdrbridge
+mingw32-make.exe clean
+qmake gdrbridge.pro
+mingw32-make.exe clean
+cd ..
+
+cd gdrexport
+mingw32-make.exe clean
+qmake gdrexport.pro
+mingw32-make.exe clean
+cd ..
+
+cd scripts
