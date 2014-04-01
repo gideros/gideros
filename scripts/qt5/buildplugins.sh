2f20ff6e61d79a5895f03eff5af27979769f3b14
Atilim Cetin <atilim.cetin@gmail.com>
2014-04-01 17:32:24 +0300

    Adjusted build scripts.

--- scripts/qt5/buildplugins.sh
+++ scripts/qt5/buildplugins.sh
@@ -3,35 +3,35 @@ cd plugins
 
 cd BitOp
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake bitop.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake bitop.pro
 make clean
 make
 cd ../..
 
 cd LuaSQLite3
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake lsqlite3.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake lsqlite3.pro
 make clean
 make
 cd ../..
 
 cd LuaSocket
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake luasocket.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake luasocket.pro
 make clean
 make
 cd ../..
 
 cd LPeg
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake lpeg.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake lpeg.pro
 make clean
 make
 cd ../..
 
 cd LuaFileSystem
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake lfs.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake lfs.pro
 make clean
 make
 cd ../..
@@ -39,14 +39,14 @@ cd ../..
 cd Microphone
 cd source
 cd Desktop
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake microphone.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake microphone.pro
 make clean
 make
 cd ../../..
 
 cd JSON
 cd source
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/qmake json.pro
+/Qt/Qt5.2.1/5.2.1/clang_64/bin/qmake json.pro
 make clean
 make
 cd ../..
