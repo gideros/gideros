c0dafcd05bbce9bc7f1ef42065f77cf4ab242ee7
Atilim Cetin <atilim.cetin@gmail.com>
2014-04-02 05:39:43 +0300

    Adjusted build scripts.

--- scripts/copyall.sh
+++ scripts/copyall.sh
@@ -7,11 +7,7 @@ cp -R ../texturepacker/Gideros\ Texture\ Packer.app/ ~/Desktop/release/Gideros\ 
 cp -R ../fontcreator/Gideros\ Font\ Creator.app/ ~/Desktop/release/Gideros\ Font\ Creator.app
 cp -R ../licensemanager/Gideros\ License\ Manager.app/ ~/Desktop/release/Gideros\ License\ Manager.app
 
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/macdeployqt ~/Desktop/release/Gideros\ Studio.app
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/macdeployqt ~/Desktop/release/Gideros\ Player.app
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/macdeployqt ~/Desktop/release/Gideros\ Texture\ Packer.app
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/macdeployqt ~/Desktop/release/Gideros\ Font\ Creator.app
-/Qt/1.2.1/Desktop/Qt/4.8.1/gcc/bin/macdeployqt ~/Desktop/release/Gideros\ License\ Manager.app
+./copyqt4.sh
 
 cp -R /Volumes/release/Documentation ~/Desktop/release/Documentation
 cp -R /Volumes/release/Examples ~/Desktop/release/Examples
