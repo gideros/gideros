a6379145e5f3602c27784cdccba026d0965648e5
Atilim Cetin <atilim.cetin@gmail.com>
2014-06-03 22:10:54 +0300

    Modifications for Qt 5.3.0.

--- scripts/copyqt5.bat
+++ scripts/copyqt5.bat
@@ -1,36 +1,36 @@
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icudt51.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icuin51.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icuuc51.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libgcc_s_dw2-1.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libstdc++-6.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libwinpthread-1.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Core.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Gui.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Network.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5OpenGL.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5PrintSupport.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Widgets.dll ..\..\release
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Xml.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icudt52.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icuin52.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icuuc52.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libgcc_s_dw2-1.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libstdc++-6.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libwinpthread-1.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Core.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Gui.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Network.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5OpenGL.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5PrintSupport.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Widgets.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Xml.dll ..\..\release
 
 mkdir ..\..\release\imageformats
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\plugins\imageformats\qjpeg.dll ..\..\release\imageformats
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\plugins\imageformats\qjpeg.dll ..\..\release\imageformats
 
 mkdir ..\..\release\platforms
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\plugins\platforms\qminimal.dll ..\..\release\platforms
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\plugins\platforms\qoffscreen.dll ..\..\release\platforms
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\plugins\platforms\qwindows.dll ..\..\release\platforms
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\plugins\platforms\qminimal.dll ..\..\release\platforms
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\plugins\platforms\qoffscreen.dll ..\..\release\platforms
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\plugins\platforms\qwindows.dll ..\..\release\platforms
 
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\lib\qscintilla2.dll ..\..\release
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\lib\qscintilla2.dll ..\..\release
 
 cp ..\libgid\external\glew-1.10.0\lib\mingw48_32\glew32.dll ..\..\release
 cp ..\libgid\external\openal-soft-1.13\build\mingw48_32\OpenAL32.dll ..\..\release
 
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icudt51.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icuin51.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\icuuc51.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libgcc_s_dw2-1.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libstdc++-6.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\libwinpthread-1.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Core.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Network.dll ..\..\release\Tools
-cp C:\Qt\Qt5.2.1\5.2.1\mingw48_32\bin\Qt5Xml.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icudt52.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icuin52.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\icuuc52.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libgcc_s_dw2-1.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libstdc++-6.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\libwinpthread-1.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Core.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Network.dll ..\..\release\Tools
+cp C:\Qt\Qt5.3.0\5.3\mingw482_32\bin\Qt5Xml.dll ..\..\release\Tools
