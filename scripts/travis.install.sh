f68c1aed73113c049d1cb4c9371fe97f0efe160e
Atilim Cetin <atilim.cetin@gmail.com>
2014-10-31 13:58:09 +0200

    Added qscintilla installation script.

--- scripts/travis.install.sh
+++ scripts/travis.install.sh
@@ -3,10 +3,13 @@ brew install jpeg
 brew install libpng
 brew install freetype
 brew install glew
+brew install wget
 brew install qt5
 cd scripts
 
 export QT=/usr/local/Cellar/qt5/5.3.2
+
+bash installqscintilla.sh
 
 bash qt5/buildqtlibs.sh
 bash qt5/buildplugins.sh
