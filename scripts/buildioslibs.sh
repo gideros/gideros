1bde1890228d95ff9caeb37acdca7ea23ab8e911
Atilim Cetin <atilim.cetin@gmail.com>
2014-10-31 14:22:41 +0200

    Added iOS build for travis.

--- scripts/buildioslibs.sh
+++ scripts/buildioslibs.sh
@@ -8,20 +8,20 @@ cd ..
 #cd ../../..
 
 cd libgvfs
-xcodebuild -alltargets -sdk iphonesimulator7.1 -configuration Release -project gvfs.xcodeproj
-xcodebuild -alltargets -sdk iphoneos7.1 -configuration Release -project gvfs.xcodeproj
+xcodebuild -alltargets -sdk iphonesimulator$IOS_SDK -configuration Release -project gvfs.xcodeproj
+xcodebuild -alltargets -sdk iphoneos$IOS_SDK -configuration Release -project gvfs.xcodeproj
 lipo build/Release-iphoneos/libgvfs.a build/Release-iphonesimulator/libgvfs.a -create -output libgvfs.a
 cd ..
 
 cd lua
-xcodebuild -alltargets -sdk iphonesimulator7.1 -configuration Release -project lua.xcodeproj
-xcodebuild -alltargets -sdk iphoneos7.1 -configuration Release -project lua.xcodeproj
+xcodebuild -alltargets -sdk iphonesimulator$IOS_SDK -configuration Release -project lua.xcodeproj
+xcodebuild -alltargets -sdk iphoneos$IOS_SDK -configuration Release -project lua.xcodeproj
 lipo build/Release-iphoneos/liblua.a build/Release-iphonesimulator/liblua.a -create -output liblua.a
 cd ..
 
 cd ios/iosplayer
-xcodebuild -alltargets -sdk iphonesimulator7.1 -configuration Release -project iosplayer.xcodeproj
-xcodebuild -alltargets -sdk iphoneos7.1 -configuration Release -project iosplayer.xcodeproj
+xcodebuild -alltargets -sdk iphonesimulator$IOS_SDK -configuration Release -project iosplayer.xcodeproj
+xcodebuild -alltargets -sdk iphoneos$IOS_SDK -configuration Release -project iosplayer.xcodeproj
 lipo build/Release-iphoneos/libiosplayer.a build/Release-iphonesimulator/libiosplayer.a -create -output libgideros.a
 cd ../..
 
--- scripts/travis.install.sh
+++ scripts/travis.install.sh
@@ -5,14 +5,16 @@ brew install freetype
 brew install glew
 brew install wget
 brew install qt5
-cd scripts
 
 export QT=/usr/local/Cellar/qt5/5.3.2
+export IOS_SDK=8.1
 
+cd scripts
 bash installqscintilla.sh
-
 bash qt5/buildqtlibs.sh
 bash qt5/buildplugins.sh
 bash qt5/cleanqt.sh
 bash qt5/buildqt.sh
+bash cleanioslibs.sh
+bash buildioslibs.sh
 
