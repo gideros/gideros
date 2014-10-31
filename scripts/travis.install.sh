642794abafbee69d50711eabd282c78061eb9f3f
Atilim Cetin <atilim.cetin@gmail.com>
2014-10-31 22:41:17 +0200

    Added building windows executables through wine.

--- scripts/travis.install.sh
+++ scripts/travis.install.sh
@@ -5,16 +5,24 @@ brew install freetype
 brew install glew
 brew install wget
 brew install qt5
+brew install wine
 
 export QT=/usr/local/Cellar/qt5/5.3.2
 export IOS_SDK=8.1
 
 cd scripts
+
 bash installqscintilla.sh
 bash qt5/buildqtlibs.sh
 bash qt5/buildplugins.sh
 bash qt5/cleanqt.sh
 bash qt5/buildqt.sh
+
 bash cleanioslibs.sh
 bash buildioslibs.sh
+
+wine cmd /c qt5\\buildqtlibs.bat
+wine cmd /c qt5\\buildplugins.bat
+wine cmd /c qt5\\cleanqt.bat
+wine cmd /c qt5\\buildqt.bat
 
