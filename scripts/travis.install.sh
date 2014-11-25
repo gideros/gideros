9e72aa9aa33868fc7b10c3e8bc86bf266e7e7c93
Atilim Cetin <atilim.cetin@gmail.com>
2014-11-25 12:52:08 +0200

    Added android SDK & NDK installation.

--- scripts/travis.install.sh
+++ scripts/travis.install.sh
@@ -1,5 +1,3 @@
-echo "Android home:" $ANDROID_HOME 
-
 brew update
 brew install jpeg
 brew install libpng
@@ -9,12 +7,23 @@ brew install wget
 brew install qt5
 brew install wine
 brew install ant
+brew install android-sdk
+brew install android-ndk
+
+expect -c '
+set timeout -1   ;
+spawn android update sdk -u -t tools,platform-tools,build-tools-21.1.1,android-21; 
+expect { 
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
 
 export QT=/usr/local/Cellar/qt5/5.3.2
 export QT_WIN=~/.wine/drive_c/Qt/Qt5.3.2
 export IOS_SDK=8.0
-export ANDROID_NDK=~/android-ndk-r10c
-export ANDROID_HOME=~/android-sdk-macosx
+export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_NDK=/usr/local/opt/android-ndk
 
 cd scripts
 
