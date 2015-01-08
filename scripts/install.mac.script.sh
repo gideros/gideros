37e542cb75c094aa88f41a8434271ef899468d95
Arturs Sosins <ar2rsawseen@gmail.com>
2015-01-08 20:52:44 +0200

    Paralelizing win build

--- scripts/install.mac.script.sh
+++ scripts/install.mac.script.sh
@@ -1,18 +1,15 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo 'Updating brew';
+echo 'Updating brew'
 brew update &> /dev/null
-echo 'Finished updating brew';
-echo 'Installing dependencies';
-(brew install freetype) &
-(brew install glew) &
-(brew install qt5) &
-(brew install ant) &
-(brew install android-ndk) &
+echo 'Finished updating brew'
+echo 'Installing dependencies'
+(brew install freetype &> /dev/null) &
+(brew install glew &> /dev/null) &
+(brew install qt5 &> /dev/null) &
+(brew install ant &> /dev/null) &
+(brew install android-sdk &> /dev/null) &
+(brew install android-ndk &> /dev/null) &
 wait
-
-echo 'Finished installing dependencies';
-
+echo 'Fiished installing dependencies'
 expect -c '
 set timeout -1   ;
 spawn android update sdk -u -t tools,platform-tools,build-tools-21.1.1,android-21; 
@@ -23,52 +20,49 @@ expect {
 '
 
 export QT=/usr/local/Cellar/qt5/5.3.2
+export QT_WIN=~/.wine/drive_c/Qt/Qt5.3.2
 export IOS_SDK=8.1
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export ANDROID_NDK=/usr/local/opt/android-ndk
 
-rm -rf $DIR/../build
-mkdir $DIR/../build
-cd $DIR
+rm -rf build
+mkdir build
 
-echo 'Building libs';
+cd scripts
 (
 echo 'Building iOS libraries...'
-bash cleanioslibs.sh &> /dev/null
-bash buildioslibs.sh &> /dev/null
-bash buildiosplugins.sh &> /dev/null
+bash cleanioslibs.sh
+bash buildioslibs.sh
+bash buildiosplugins.sh
 ) &
 (
 echo 'Building Android libraries...'
-bash makejar.sh &> /dev/null
-bash buildandroidlibs.sh &> /dev/null
-bash buildandroidso.sh &> /dev/null
-bash buildandroidplugins.sh &> /dev/null
+bash makejar.sh
+bash buildandroidlibs.sh
+bash buildandroidso.sh
+bash buildandroidplugins.sh
 ) &
 (
 echo 'Installing QScintilla for Mac...'
-bash downloadqscintilla.sh &> /dev/null
-bash extractqscintilla.sh &> /dev/null
-bash installqscintilla.sh &> /dev/null
+bash downloadqscintilla.sh
+bash extractqscintilla.sh
+bash installqscintilla.sh
 ) &
 (
 echo 'Building Qt applications for Mac...'
-rm -rf Sdk
-bash qt5/buildqtlibs.sh &> /dev/null
-bash qt5/buildplugins.sh &> /dev/null
-bash qt5/cleanqt.sh &> /dev/null
-bash qt5/buildqt.sh &> /dev/null
+rm -rf ../Sdk
+bash qt5/buildqtlibs.sh
+bash qt5/buildplugins.sh
+bash qt5/cleanqt.sh
+bash qt5/buildqt.sh
 ) &
 wait
-
-echo 'Finished building libs';
 
 echo 'Copying Mac files...'
 bash copymac.sh
 
 echo 'Creating Mac installation package...'
 bash createmacpackage.sh
-
 
 
 
--- scripts/install.win.script.sh
+++ scripts/install.win.script.sh
@@ -1,11 +1,15 @@
+echo 'Updating brew'
 brew update &> /dev/null
-brew install freetype &> /dev/null
-brew install glew &> /dev/null
-brew install wine &> /dev/null
-brew install ant &> /dev/null
-brew install android-sdk &> /dev/null
-brew install android-ndk &> /dev/null
-
+echo 'Finished updating brew'
+echo 'Installing dependencies'
+(brew install freetype &> /dev/null) &
+(brew install glew &> /dev/null) &
+(brew install wine &> /dev/null) &
+(brew install ant &> /dev/null) &
+(brew install android-sdk &> /dev/null) &
+(brew install android-ndk &> /dev/null) &
+wait
+echo 'Fiished installing dependencies'
 rm -rf ~/.wine
 wine xyz 
 
@@ -28,35 +32,42 @@ rm -rf build
 mkdir build
 
 cd scripts
-
+(
 echo 'Installing Qt for Windows...'
 bash installwinqt.sh
-
+) &
+(
 echo 'Installing NSIS for Windows...'
 bash installnsis.sh
-
+) &
+(
 echo 'Installing QScintilla for Windows...'
 bash downloadqscintilla.sh
 bash extractqscintilla.sh
 wine cmd /c installqscintilla.bat
-
+) &
+(
 echo 'Building Qt applications for Windows...'
 rm -rf ../Sdk
 wine cmd /c qt5\\buildqtlibs.bat
 wine cmd /c qt5\\buildplugins.bat
 wine cmd /c qt5\\cleanqt.bat
 wine cmd /c qt5\\buildqt.bat
-
+) &
+(
 echo 'Building iOS libraries...'
 bash cleanioslibs.sh
 bash buildioslibs.sh
 bash buildiosplugins.sh
-
+) &
+(
 echo 'Building Android libraries...'
 bash makejar.sh
 bash buildandroidlibs.sh
 bash buildandroidso.sh
 bash buildandroidplugins.sh
+) &
+wait
 
 echo 'Copying Windows files...'
 bash copywin.sh
