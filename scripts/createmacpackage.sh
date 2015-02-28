0cbe2ab393fea3e8ab16d205ea600ef91ce1090b
Arturs Sosins <ar2rsawseen@gmail.com>
2015-02-28 14:04:02 +0200

    Removed version from end file

--- scripts/createmacpackage.sh
+++ scripts/createmacpackage.sh
@@ -4,7 +4,7 @@ mkdir build/Gideros
 mv build/mac build/Gideros/Gideros\ Studio
 ln -s /Applications build/Gideros/Applications
 
-rm build/gideros_$GVERSION.dmg
-hdiutil create build/gideros_$GVERSION.dmg -srcfolder build/Gideros
+rm build/gideros.dmg
+hdiutil create build/gideros.dmg -srcfolder build/Gideros
 
 cd scripts
--- scripts/createwinpackage.sh
+++ scripts/createwinpackage.sh
@@ -1,5 +1,5 @@
 cp ../Release/gideros_mui2.nsi ../build/win
 cd ../build/win
 wine "C:\\Program Files\\NSIS\\makensis.exe" gideros_mui2.nsi
-mv gideros.exe ../gideros_$GVERSION.exe
+mv gideros.exe ../gideros.exe
 cd ../../scripts
