764784ecf00ba485b6bf60efb0765af8c10e0a3d
Arturs Sosins <ar2rsawseen@gmail.com>
2015-02-28 16:02:29 +0200

    Win script removing android

--- scripts/install.win.script.sh
+++ scripts/install.win.script.sh
@@ -5,28 +5,9 @@ echo 'Installing dependencies'
 brew install freetype --universal
 brew install glew
 brew install wine
-brew install ant
-brew install android-sdk
-brew install android-ndk
-wait
-echo 'Finished installing dependencies'
-rm -rf ~/.wine
-wine xyz 
-
-expect -c '
-set timeout -1   ;
-spawn android update sdk -u -a -t tool,platform-tool,3,android-21;
-expect { 
-    "Do you accept the license" { exp_send "y\r" ; exp_continue }
-    eof
-}
-'
 
 export QT=/usr/local/Cellar/qt5/5.4.0
 export QT_WIN=~/.wine/drive_c/Qt/Qt5.3.2
-export IOS_SDK=8.1
-export ANDROID_HOME=/usr/local/opt/android-sdk
-export ANDROID_NDK=/usr/local/opt/android-ndk
 
 rm -rf build
 mkdir build
@@ -49,17 +30,6 @@ wine cmd /c qt5\\buildqtlibs.bat
 wine cmd /c qt5\\buildplugins.bat
 wine cmd /c qt5\\cleanqt.bat
 wine cmd /c qt5\\buildqt.bat
-
-#echo 'Building iOS libraries...'
-#bash cleanioslibs.sh
-#bash buildioslibs.sh
-#bash buildiosplugins.sh
-
-echo 'Building Android libraries...'
-bash makejar.sh
-bash buildandroidlibs.sh
-bash buildandroidso.sh
-bash buildandroidplugins.sh
 
 echo 'Copying Windows files...'
 bash copywin.sh
