1e3d2871c72a9821f01d26756d117bf500366ecb
Arturs Sosins <ar2rsawseen@gmail.com>
2015-02-28 16:02:19 +0200

    Mac script outputing only errors

--- scripts/install.mac.script.sh
+++ scripts/install.mac.script.sh
@@ -1,13 +1,13 @@
 echo 'Updating brew'
-brew update &> /dev/null
+brew update  > /dev/null
 echo 'Finished updating brew'
 echo 'Installing dependencies'
 brew install freetype --with-freetype-dir=/usr/local/Cellar/freetype &> /dev/null
-brew install glew &> /dev/null
-brew install qt5 &> /dev/null
-brew install ant &> /dev/null
-brew install android-sdk &> /dev/null
-brew install android-ndk &> /dev/null
+brew install glew > /dev/null
+brew install qt5 > /dev/null
+brew install ant > /dev/null
+brew install android-sdk > /dev/null
+brew install android-ndk > /dev/null
 echo 'Finished installing dependencies'
 expect -c '
 set timeout -1   ;
@@ -29,27 +29,27 @@ mkdir build
 
 cd scripts
 echo 'Building iOS libraries...'
-bash cleanioslibs.sh &> /dev/null
-bash buildioslibs.sh &> /dev/null
-bash buildiosplugins.sh &> /dev/null
+bash cleanioslibs.sh > /dev/null
+bash buildioslibs.sh > /dev/null
+bash buildiosplugins.sh > /dev/null
 
 echo 'Building Android libraries...'
-bash makejar.sh
-bash buildandroidlibs.sh
-bash buildandroidso.sh
-bash buildandroidplugins.sh
+bash makejar.sh > /dev/null
+bash buildandroidlibs.sh > /dev/null
+bash buildandroidso.sh > /dev/null
+bash buildandroidplugins.sh > /dev/null
 
 echo 'Installing QScintilla for Mac...'
-bash downloadqscintilla.sh &> /dev/null
-bash extractqscintilla.sh &> /dev/null
-bash installqscintilla.sh &> /dev/null
+bash downloadqscintilla.sh > /dev/null
+bash extractqscintilla.sh > /dev/null
+bash installqscintilla.sh > /dev/null
 
 echo 'Building Qt applications for Mac...'
 rm -rf ../Sdk
-bash qt5/buildqtlibs.sh
-bash qt5/buildplugins.sh
-bash qt5/cleanqt.sh
-bash qt5/buildqt.sh
+bash qt5/buildqtlibs.sh > /dev/null
+bash qt5/buildplugins.sh > /dev/null
+bash qt5/cleanqt.sh > /dev/null
+bash qt5/buildqt.sh > /dev/null
 
 echo 'Copying Mac files...'
 bash copymac.sh
