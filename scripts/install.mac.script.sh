478861f64205f2d2214f7fd537507b2ad79e032a
Arturs Sosins <ar2rsawseen@gmail.com>
2015-02-28 11:44:01 +0200

    Updated install scripts

--- scripts/install.mac.script.sh
+++ scripts/install.mac.script.sh
@@ -1,18 +1,17 @@
 echo 'Updating brew'
-brew update &> /dev/null
+brew update
 echo 'Finished updating brew'
 echo 'Installing dependencies'
-(brew install freetype &> /dev/null) &
-(brew install glew &> /dev/null) &
-(brew install qt5 &> /dev/null) &
-(brew install ant &> /dev/null) &
-(brew install android-sdk &> /dev/null
-brew install android-ndk &> /dev/null) &
-wait
+brew install freetype --with-freetype-dir=/usr/local/Cellar/freetype
+brew install glew
+brew install qt5
+brew install ant
+brew install android-sdk
+brew install android-ndk
 echo 'Finished installing dependencies'
 expect -c '
 set timeout -1   ;
-spawn android update sdk -u -a -t 1,2,3;
+android update sdk -u -a -t 1,2,3,android-21;
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
@@ -20,7 +19,6 @@ expect {
 '
 
 export QT=/usr/local/Cellar/qt5/5.4.0
-export QT_WIN=~/.wine/drive_c/Qt/Qt5.3.2
 export IOS_SDK=8.1
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export ANDROID_NDK=/usr/local/opt/android-ndk
@@ -29,20 +27,17 @@ rm -rf build
 mkdir build
 
 cd scripts
-(
 echo 'Building iOS libraries...'
 bash cleanioslibs.sh &> /dev/null
 bash buildioslibs.sh &> /dev/null
 bash buildiosplugins.sh &> /dev/null
-) &
-(
+
 echo 'Building Android libraries...'
 bash makejar.sh
 bash buildandroidlibs.sh
 bash buildandroidso.sh
 bash buildandroidplugins.sh
-) &
-(
+
 echo 'Installing QScintilla for Mac...'
 bash downloadqscintilla.sh &> /dev/null
 bash extractqscintilla.sh &> /dev/null
@@ -54,8 +49,6 @@ bash qt5/buildqtlibs.sh
 bash qt5/buildplugins.sh
 bash qt5/cleanqt.sh
 bash qt5/buildqt.sh
-) &
-wait
 
 echo 'Copying Mac files...'
 bash copymac.sh
--- scripts/install.script.sh
+++ scripts/install.script.sh
@@ -21,7 +21,7 @@ wine xyz
 
 expect -c '
 set timeout -1   ;
-spawn android update sdk -u -a -t 1,2,3; 
+android update sdk -u -a -t 1,2,3,android-21; 
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
