f136437f6c99f809bbb360be8a74b196cae48851
Arturs Sosins <ar2rsawseen@gmail.com>
2015-03-15 20:33:33 +0200

    [build] fixed android plugin paths

--- plugins/struct/source/jni/Android.mk
+++ plugins/struct/source/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE            := lua
-LOCAL_SRC_FILES         := ../../../../../Sdk/lib/android/$(TARGET_ARCH_ABI)/liblua.so
+LOCAL_SRC_FILES         := ../../../../Sdk/lib/android/$(TARGET_ARCH_ABI)/liblua.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -14,7 +14,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE            := gideros
-LOCAL_SRC_FILES         := ../../../../../Sdk/lib/android/$(TARGET_ARCH_ABI)/libgideros.so
+LOCAL_SRC_FILES         := ../../../../Sdk/lib/android/$(TARGET_ARCH_ABI)/libgideros.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
@@ -31,7 +31,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE           := struct
 LOCAL_ARM_MODE         := arm
 LOCAL_CFLAGS           := -O2
-LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../../../../Sdk/include $(LOCAL_PATH)/..
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../../../Sdk/include $(LOCAL_PATH)/..
 LOCAL_SRC_FILES        := struct.c struct_stub.cpp
 LOCAL_LDLIBS           := -ldl -llog
 LOCAL_SHARED_LIBRARIES := lua gideros
