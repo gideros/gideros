65dc35a11881ef1f376dae55eaf96da9c4c77d85
Arturs Sosins <ar2rsawseen@gmail.com>
2015-04-07 20:02:45 +0300

    [android] blak screen after immersive view fix

--- android/GiderosAndroidPlayer/src/com/giderosmobile/android/player/GiderosAndroidPlayerActivity.java
+++ android/GiderosAndroidPlayer/src/com/giderosmobile/android/player/GiderosAndroidPlayerActivity.java
@@ -135,10 +135,12 @@ public class GiderosAndroidPlayerActivity extends Activity implements OnTouchLis
 		
 		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
 	        if (hasFocus) {
-	        	getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-	                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-	                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-	                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	        	getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	        			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	        			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	        			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	        			| View.SYSTEM_UI_FLAG_FULLSCREEN
+	        			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 	        }
 	    }
 		
