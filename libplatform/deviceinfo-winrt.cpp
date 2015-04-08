26bdc17e571e170293039d209ad2a2108eb4b38d
Arturs Sosins <ar2rsawseen@gmail.com>
2015-04-08 21:44:00 +0300

    [winrt] deviceinfo

--- libplatform/deviceinfo-winrt.cpp
+++ libplatform/deviceinfo-winrt.cpp
@@ -7,5 +7,11 @@ std::vector<std::string> getDeviceInfo()
 
 	result.push_back("WinRT");
 
+#if WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP
+	result.push_back("Windows Phone");
+#else
+	result.push_back("Windows");
+#endif
+
 	return result;
 }
