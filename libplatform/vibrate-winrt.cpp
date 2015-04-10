58b48616ba0fff0bad01fed8ae4a145463f17103
Arturs Sosins <ar2rsawseen@gmail.com>
2015-04-10 20:22:02 +0300

    [winrt] vibration stub

---
+++ libplatform/vibrate-winrt.cpp
@@ -0,0 +1,6 @@
+//using namespace Windows::Phone::Devices::Notification;
+//VibrationDevice testVibrationDevice = VibrationDevice.GetDefault();
+void vibrate()
+{
+	//testVibrationDevice.Vibrate(TimeSpan.FromSeconds(3));
+}
