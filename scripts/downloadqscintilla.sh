36783b15f95d93ff73004a2e5ae5d5d4ed518eb5
Arturs Sosins <ar2rsawseen@gmail.com>
2015-04-26 19:12:14 +0300

    [scripts] update qscintilla

--- scripts/downloadqscintilla.sh
+++ scripts/downloadqscintilla.sh
@@ -1,4 +1,4 @@
 cd ../build
-rm QScintilla-gpl-2.8.4.tar.gz
-wget http://sourceforge.net/projects/pyqt/files/QScintilla2/QScintilla-2.8.4/QScintilla-gpl-2.8.4.tar.gz
+rm QScintilla-gpl-2.9.tar.gz
+wget http://sourceforge.net/projects/pyqt/files/QScintilla2/QScintilla-gpl-2.9/QScintilla-gpl-2.9.tar.gz
 cd ../scripts
