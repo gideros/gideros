ce64a3c245f25e3a9323a225a8658866ec63f6a5
John Blackburn <john.blackburn75@gmail.com>
2015-05-19 16:21:52 +0100

    [winrt] Improve export code, no wildcards
    Wildcards cause Visual Studio to be very slow when opening project
    files. Therefore vcxproj files now contain explicit references to all
    users asset files.

--- winrt_example/giderosgame/giderosgame.Windows/Assets/main.lua
+++ winrt_example/giderosgame/giderosgame.Windows/Assets/main.lua
@@ -1,73 +1,188 @@
---[[ 
+--[[
 
-Drag the shapes around with your mouse or fingers
+Shapes example
 
 This code is MIT licensed, see http://www.opensource.org/licenses/mit-license.php
 (C) 2010 - 2011 Gideros Mobile 
 
 ]]
 
-local function onMouseDown(self, event)
-	if self:hitTestPoint(event.x, event.y) then
-		self.isFocus = true
-
-		self.x0 = event.x
-		self.y0 = event.y
-
-		event:stopPropagation()
-	end
-end
-
-local function onMouseMove(self, event)
-	if self.isFocus then
-		local dx = event.x - self.x0
-		local dy = event.y - self.y0
-		
-		self:setX(self:getX() + dx)
-		self:setY(self:getY() + dy)
-
-		self.x0 = event.x
-		self.y0 = event.y
-
-		event:stopPropagation()
-	end
-end
-
-local function onMouseUp(self, event)
-	if self.isFocus then
-		self.isFocus = false
-		event:stopPropagation()
-	end
-end
-
-for i=1,5 do
+function createShape1()
 	local shape = Shape.new()
-	
-	shape:setLineStyle(3, 0x000000)
-	shape:setFillStyle(Shape.SOLID, 0xff0000, 0.5)
+
+	shape:setLineStyle(4, 0x000000)
+
 	shape:beginPath()
-	shape:moveTo(0, 0)
-	shape:lineTo(100, 0)
-	shape:lineTo(100, 50)
-	shape:lineTo(0, 50)
+	shape:moveTo(160, 50)
+	shape:lineTo(74, 284)
+	shape:lineTo(272, 130)
+	shape:lineTo(48, 130)
+	shape:lineTo(246, 284)
 	shape:closePath()
 	shape:endPath()
 
-	shape:setX(math.random(0, 320 - 100))
-	shape:setY(math.random(0, 480 - 50))
+	local t1 = TextField.new(nil, "setLineStyle(4, 0x000000)")
+	t1:setX(60)
+	t1:setY(320)
+	shape:addChild(t1)
 
-	shape.isFocus = false
-
-	shape:addEventListener(Event.MOUSE_DOWN, onMouseDown, shape)
-	shape:addEventListener(Event.MOUSE_MOVE, onMouseMove, shape)
-	shape:addEventListener(Event.MOUSE_UP, onMouseUp, shape)
-
-	stage:addChild(shape)
+	return shape
 end
 
 
-local info = TextField.new(nil, "drag the shapes around with your mouse or fingers")
-info:setPosition(23, 50)
-stage:addChild(info)
+
+function createShape2()
+	local shape = Shape.new()
+
+	shape:setFillStyle(Shape.SOLID, 0xff0000, 0.5)
+
+	shape:beginPath()
+	shape:moveTo(160, 50)
+	shape:lineTo(74, 284)
+	shape:lineTo(272, 130)
+	shape:lineTo(48, 130)
+	shape:lineTo(246, 284)
+	shape:closePath()
+	shape:endPath()
+
+	local t1 = TextField.new(nil, "setFillStyle(Shape.SOLID, 0xff0000, 0.5)")
+	t1:setX(60)
+	t1:setY(320)
+	shape:addChild(t1)
+
+	return shape
+end
 
 
+function createShape3()
+	local shape = Shape.new()
+
+	shape:setFillStyle(Shape.SOLID, 0xff0000, 0.5)
+
+	shape:beginPath(Shape.NON_ZERO)
+	shape:moveTo(160, 50)
+	shape:lineTo(74, 284)
+	shape:lineTo(272, 130)
+	shape:lineTo(48, 130)
+	shape:lineTo(246, 284)
+	shape:closePath()
+	shape:endPath()
+
+	local t1 = TextField.new(nil, "setFillStyle(Shape.SOLID, 0xff0000, 0.5)")
+	t1:setX(60)
+	t1:setY(320)
+	shape:addChild(t1)
+
+	local t2 = TextField.new(nil, "winding = Shape.NON_ZERO")
+	t2:setX(60)
+	t2:setY(332)
+	shape:addChild(t2)
+
+	return shape
+end
+
+function createShape4()
+	local shape = Shape.new()
+
+	shape:setLineStyle(4, 0x000000)
+	shape:setFillStyle(Shape.SOLID, 0xff0000, 0.5)
+
+	shape:beginPath()
+	shape:moveTo(160, 50)
+	shape:lineTo(74, 284)
+	shape:lineTo(272, 130)
+	shape:lineTo(48, 130)
+	shape:lineTo(246, 284)
+	shape:closePath()
+	shape:endPath()
+
+	local t1 = TextField.new(nil, "setLineStyle(4, 0x000000)")
+	t1:setX(60)
+	t1:setY(320)
+	shape:addChild(t1)
+
+	local t2 = TextField.new(nil, "setFillStyle(Shape.SOLID, 0xff0000, 0.5)")
+	t2:setX(60)
+	t2:setY(332)
+	shape:addChild(t2)
+
+	return shape
+end
+
+
+function createShape5()
+	local texture = Texture.new("spots.png")
+	local matrix = Matrix.new(1, 0, 0, 1, 48, 50)
+
+	local shape = Shape.new()
+
+	shape:setFillStyle(Shape.TEXTURE, texture, matrix)
+
+	shape:beginPath(Shape.NON_ZERO)
+	shape:moveTo(160, 50)
+	shape:lineTo(74, 284)
+	shape:lineTo(272, 130)
+	shape:lineTo(48, 130)
+	shape:lineTo(246, 284)
+	shape:closePath()
+	shape:endPath()
+
+	local t1 = TextField.new(nil, "setFillStyle(Shape.TEXTURE, texture, matrix)")
+	t1:setX(60)
+	t1:setY(320)
+	shape:addChild(t1)
+
+	local t2 = TextField.new(nil, "winding = Shape.NON_ZERO")
+	t2:setX(60)
+	t2:setY(332)
+	shape:addChild(t2)
+
+	return shape
+end
+
+local shapes = {
+	createShape1(),
+	createShape2(),
+	createShape3(),
+	createShape4(),
+	createShape5(),
+}
+
+local current = 1
+stage:addChild(shapes[current])
+
+local leftup = Bitmap.new(Texture.new("left-up.png"))
+local leftdown = Bitmap.new(Texture.new("left-down.png"))
+local left = Button.new(leftup, leftdown)
+left:setPosition(106, 400)
+stage:addChild(left)
+
+
+local rightup = Bitmap.new(Texture.new("right-up.png"))
+local rightdown = Bitmap.new(Texture.new("right-down.png"))
+local right = Button.new(rightup, rightdown)
+right:setPosition(166, 400)
+stage:addChild(right)
+
+function onClick(event)
+	local next = current
+	
+	if event:getTarget() == left then
+		next = next - 1
+		if next < 1 then
+			next = #shapes
+		end
+	elseif event:getTarget() == right then
+		next = next + 1
+		if next > #shapes then
+			next = 1
+		end
+	end
+	
+	stage:removeChild(shapes[current])
+	stage:addChild(shapes[next])
+	current = next
+end
+
+left:addEventListener("click", onClick)
+right:addEventListener("click", onClick)
