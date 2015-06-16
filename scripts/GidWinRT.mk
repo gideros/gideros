8bdb9aaa7c5c7fa7042f455c4cf601e59e016e42
Nicolas Bouquet <hgy01@hieroglyphe.net>
2015-06-16 14:42:32 +0200

    Split Makefile for gideros for easier management Correct clean target for Makefile

---
+++ scripts/GidWinRT.mk
@@ -0,0 +1,16 @@
+WINRT_SHADERS=Basic Color Texture TextureColor
+WINRT_SHADERS_PATH=2dsg/gfxbackends/dx11
+WINRT_SHADERS_FILE=dx11_shaders.c
+BIN2C=$(ROOT)/scripts/bin2c
+
+winrt.shaders: $(BIN2C)
+	rm -f $(WINRT_SHADERS_PATH)/$(WINRT_SHADERS_FILE)
+	for s in $(WINRT_SHADERS); do $(FXC) //T vs_4_0_level_9_3 //E VShader //Fo $(WINRT_SHADERS_PATH)/v$$s.cso $(WINRT_SHADERS_PATH)/$$s.hlsl; done
+	for s in $(WINRT_SHADERS); do $(FXC) //T ps_4_0_level_9_3 //E PShader //Fo $(WINRT_SHADERS_PATH)/p$$s.cso $(WINRT_SHADERS_PATH)/$$s.hlsl; done
+	P=$(PWD); cd $(WINRT_SHADERS_PATH); for s in $(WINRT_SHADERS); do $$P/$(BIN2C) p$$s.cso >> $(WINRT_SHADERS_FILE); $$P/$(BIN2C) v$$s.cso >> $(WINRT_SHADERS_FILE); done
+	rm -f $(WINRT_SHADERS_PATH)/*.cso	
+	#cp winrt/*.cso winrt_example/giderosgame/giderosgame.Windows/Assets
+	#cp winrt/*.cso winrt_example/giderosgame/giderosgame.WindowsPhone/Assets
+	#cp winrt/*.cso "ui/Templates/VisualStudio/WinRT Template/giderosgame/giderosgame.Windows/Assets"
+	#cp winrt/*.cso "ui/Templates/VisualStudio/WinRT Template/giderosgame/giderosgame.WindowsPhone/Assets"
+	
