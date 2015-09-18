bd6d9a0dcab1a90552690cf2edfc05f214833b83
Antonio Calatrava <antonio@antoniocalatrava.com>
2015-09-18 11:00:55 +0200

    AppleTV support
    Dropping system call since it is not allowed on AppleTV

--- lua/src/loslib.c
+++ lua/src/loslib.c
@@ -37,7 +37,9 @@ static int os_pushresult (lua_State *L, int i, const char *filename) {
 
 
 static int os_execute (lua_State *L) {
+  #ifndef TARGET_OS_TV
   lua_pushinteger(L, system(luaL_optstring(L, 1, NULL)));
+  #endif
   return 1;
 }
 
