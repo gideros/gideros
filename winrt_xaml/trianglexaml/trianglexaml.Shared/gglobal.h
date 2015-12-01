0ac8f1d1a7cacc0fa995a923ee7fcea95a43e92d
Arturs Sosins <ar2rsawseen@gmail.com>
2015-12-01 20:03:12 +0200

    [winrt_xaml] add gideros plugins headers

---
+++ winrt_xaml/trianglexaml/trianglexaml.Shared/gglobal.h
@@ -0,0 +1,22 @@
+#ifndef _GGLOBAL_H_
+#define _GGLOBAL_H_
+
+#include <gexport.h>
+
+typedef unsigned long g_id;
+
+typedef int g_bool;
+#define g_false (0)
+#define g_true (1)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+G_API g_id g_NextId();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _GGLOBAL_H_
---
+++ winrt_xaml/trianglexaml/trianglexaml.Shared/giderosapi.h
@@ -0,0 +1,41 @@
+#ifndef GIDEROSAPI_H
+#define GIDEROSAPI_H
+
+using namespace Windows::UI::Core;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void gdr_initialize(bool useXaml, CoreWindow^ Window, Windows::UI::Xaml::Controls::SwapChainPanel^ swapChainPanel,
+						int width, int height, bool player, const wchar_t* resourcePath, const wchar_t* docsPath, const wchar_t* tempPath);
+	void gdr_drawFirstFrame();
+	void gdr_drawFrame(bool useXaml);
+	void gdr_exitGameLoop();
+	void gdr_deinitialize();
+	void gdr_suspend();
+	void gdr_resume();
+	void gdr_didReceiveMemoryWarning();
+	void gdr_foreground();
+	void gdr_background();
+	void gdr_openProject(const char* project);
+	bool gdr_isRunning();
+	void gdr_keyDown(int keyCode);
+	void gdr_keyUp(int keyCode);
+	void gdr_mouseDown(int x, int y, int button);
+	void gdr_mouseMove(int x, int y);
+	void gdr_mouseHover(int x, int y);
+	void gdr_mouseUp(int x, int y, int button);
+	void gdr_mouseWheel(int x, int y, int delta);
+	void gdr_touchBegin(int x, int y, int id);
+	void gdr_touchMove(int x, int y, int id);
+	void gdr_touchEnd(int x, int y, int id);
+	void gdr_touchCancel(int x, int y, int id);
+	void gdr_resize(int width, int height);
+	Windows::UI::Xaml::Controls::SwapChainPanel^ gdr_getRootView();
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
