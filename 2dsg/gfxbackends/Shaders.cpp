dc39d5f5926fc23cf3712c1fb6e2d6d66ce1cd4b
Nicolas Bouquet <hgy01@hieroglyphe.net>
2015-12-02 10:24:02 +0100

    Fix clipping bounds on rotated matrices

--- 2dsg/gfxbackends/Shaders.cpp
+++ 2dsg/gfxbackends/Shaders.cpp
@@ -169,29 +169,24 @@ Matrix4 ShaderEngine::setOrthoFrustum(float l, float r, float b, float t, float 
 }
 
 
-//Coordinates are untransformed coordinates (ie, cuurent Sprite local)
+//Coordinates are untransformed coordinates (ie, current Sprite local)
 void ShaderEngine::pushClip(float x,float y,float w,float h)
 {
 	Vector4 v1(x,y,0,1);
 	Vector4 v2(x+w,y+h,0,1);
+	Vector4 v3(x+w,y,0,1);
+	Vector4 v4(x,y+h,0,1);
 	Matrix4 xform=oglVPProjection*oglModel;
 	v1=xform*v1;
 	v2=xform*v2;
-	//glog_d("Scissor: [%f,%f->%f,%f] -> [%f,%f->%f,%f]",x,y,x+w,y+h,v1.x,v1.y,v2.x,v2.y);
-	x=v1.x;
-	w=v2.x-v1.x;
-	if (w<0)
-	{
-		x=v2.x;
-		w=-w;
-	}
-	y=v1.y;
-	h=v2.y-v1.y;
-	if (h<0)
-	{
-		y=v2.y;
-		h=-h;
-	}
+	v3=xform*v3;
+	v4=xform*v4;
+	x=(std::min)((std::min)(v1.x,v2.x),(std::min)(v3.x,v4.x));
+	y=(std::min)((std::min)(v1.y,v2.y),(std::min)(v3.y,v4.y));
+	float x2=(std::max)((std::max)(v1.x,v2.x),(std::max)(v3.x,v4.x));
+	float y2=(std::max)((std::max)(v1.y,v2.y),(std::max)(v3.y,v4.y));
+	w=x2-x;
+	h=y2-y;
 	if (scissorStack.empty())
 	{
 		Scissor s(x,y,w,h);
