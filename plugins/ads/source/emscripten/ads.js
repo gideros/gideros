42402197610e5cf1d512af70509ce792c9ff70da
Arturs Sosins <ar2rsawseen@gmail.com>
2015-12-04 21:01:35 +0200

    [html] adding google adsense

--- plugins/ads/source/emscripten/ads.js
+++ plugins/ads/source/emscripten/ads.js
@@ -15,37 +15,88 @@ window.onclick=function()
 GiderosAds.Init=function(){};
 GiderosAds.Deinit=function(){};
 
+GiderosAds.frameworks = {};
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 GiderosAds.Initialize=function(ad) {
-	 (function(d, s, id){
+	 (function(d, s, id, callback){
 	      var js, fjs = d.getElementsByTagName(s)[0];
 	      if (d.getElementById(id)) {return;}
 	      js = d.createElement(s); js.id = id;
-	      js.src = 'Ads'+ad+'.js';
+	      js.src = id+'.js';
+          js.onreadystatechange = callback;
+          js.onload = callback;
 	      fjs.parentNode.insertBefore(js, fjs);
-	 }(document, 'script', 'Ads'+ad));
+	 }(document, 'script', 'Ads'+capitalizeFirstLetter(ad), function(){
+         if(!GiderosAds.frameworks[ad])
+            GiderosAds.frameworks[ad] = new window['Ads'+capitalizeFirstLetter(ad)]();
+     }));
 };
 
 GiderosAds.Destroy=function(ad) 
 { 
+    delete GiderosAds.frameworks[ad];
 	 (function(d, s, id){
 	      var e=d.getElementById(id);
 	      if (e)
 	    	  e.parentNode.removeNode(e);
-	 }(document, 'script', 'Ads'+ad));
+	 }(document, 'script', 'Ads'+capitalizeFirstLetter(ad)));
 };
 
-GiderosAds.EnableTesting=function(ad) { };
-GiderosAds.SetKey=function(ad,params) { };
-GiderosAds.LoadAd=function(ad,params) { };
-GiderosAds.ShowAd=function(ad,params) { };
-GiderosAds.HideAd=function(ad,type) { };
-GiderosAds.SetAlignment=function(ad,hor,ver) { };
-GiderosAds.SetX=function(ad,v) { };
-GiderosAds.SetY=function(ad,v) { };
-GiderosAds.GetX=function(ad) { };
-GiderosAds.GetY=function(ad) { };
-GiderosAds.GetWidth=function(ad) { };
-GiderosAds.GetHeight=function(ad) { };
+GiderosAds.EnableTesting=function(ad) { 
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].enableTesting();
+};
+GiderosAds.SetKey=function(ad,params) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].setKey(params);
+};
+GiderosAds.LoadAd=function(ad,params) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].loadAd(params);
+};
+GiderosAds.ShowAd=function(ad,params) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].showAd(params);
+};
+GiderosAds.HideAd=function(ad,type) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].hideAd(params);
+};
+GiderosAds.SetAlignment=function(ad,hor,ver) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].setAlignment(hor,ver);
+};
+GiderosAds.SetX=function(ad,v) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].setX(v);
+};
+GiderosAds.SetY=function(ad,v) {
+    if(GiderosAds.frameworks[ad])
+        GiderosAds.frameworks[ad].setY(v);
+};
+GiderosAds.GetX=function(ad) {
+    if(GiderosAds.frameworks[ad])
+        return GiderosAds.frameworks[ad].getX();
+    return 0;
+};
+GiderosAds.GetY=function(ad) {
+    if(GiderosAds.frameworks[ad])
+        return GiderosAds.frameworks[ad].getY();
+    return 0;
+};
+GiderosAds.GetWidth=function(ad) {
+    if(GiderosAds.frameworks[ad])
+        return GiderosAds.frameworks[ad].getWidth();
+    return 0;
+};
+GiderosAds.GetHeight=function(ad) {
+    if(GiderosAds.frameworks[ad])
+        return GiderosAds.frameworks[ad].getHeight();
+    return 0;
+};
 
 GiderosAds.onAdReceived=function(ad,type) {
     Module.ccall('gads_onAdReceived',null,['string','string'],[ ad,type ]);	
@@ -73,3 +124,6 @@ GiderosAds.onAdError=function(ad,error) {
 GiderosAds.Login=function(appid,params)
 {
 };*/
+
+
+
