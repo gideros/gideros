ac26493ed327db91eeb67881ce81ab2896599612
Arturs Sosins <ar2rsawseen@gmail.com>
2015-12-06 14:00:13 +0200

    [winrt] basic pubcenter ads support

--- winrt_xaml/giderosxaml/giderosxaml.WindowsPhone/Plugins/Ads/frameworks/AdsPubcenter.h
+++ winrt_xaml/giderosxaml/giderosxaml.WindowsPhone/Plugins/Ads/frameworks/AdsPubcenter.h
@@ -1,6 +1,26 @@
-class AdsPubcenter
+#include "../ads.h"
+#include "../AdsAbstract.h"
+class AdsPubcenter : public AdsAbstract
 {
 public:
 	AdsPubcenter();
 	~AdsPubcenter();
+	void setKey(gads_Parameter *params);
+	void enableTesting();
+	void loadAd(gads_Parameter *params);
+	void showAd(gads_Parameter *params);
+	void hideAd(const char* type);
+	void setAlignment(const char *hor, const char *verr);
+	void setX(int x);
+	void setY(int y);
+	int getX();
+	int getY();
+	int getWidth();
+	int getHeight();
+private:
+	std::string key;
+	std::string curSize = "250x250";
+	std::map<std::string, std::pair<int, int>> adSizes;
+	std::map<std::string, std::string> testUnits;
+	bool test = false;
 };
