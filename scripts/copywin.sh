dcb49c080e682f62171575adeb46ed5f0012896c
ar2rsawseen <ar2rsawseen@gmail.com>
2016-01-29 10:34:21 +0200

    [scripts] copy apt template

--- scripts/copywin.sh
+++ scripts/copywin.sh
@@ -79,6 +79,10 @@ git archive -o $BUILD_DIR/tmp.tar HEAD:ui/Templates/Xcode4
 mkdir $BUILD_DIR/win/Templates/Xcode4
 tar xf $BUILD_DIR/tmp.tar -C $BUILD_DIR/win/Templates/Xcode4
 
+git archive -o $BUILD_DIR/tmp.tar HEAD:ui/Templates/APK
+mkdir $BUILD_DIR/win/Templates/APK
+tar xf $BUILD_DIR/tmp.tar -C $BUILD_DIR/win/Templates/APK
+
 mkdir $BUILD_DIR/win/Templates/Eclipse/Android\ Template/assets
 mkdir $BUILD_DIR/win/Templates/Eclipse/Android\ Template/gen
 mkdir $BUILD_DIR/win/Templates/Eclipse/Android\ Template/res/layout
