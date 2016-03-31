f299bbbedca31b083683b991b502d007a9bdb1df
John Blackburn <john.blackburn75@gmail.com>
2016-03-31 18:50:11 +0100

    [win32] Start to add AlertDialog and TextInputDialog

--- libgid/src/win32/gui-win32.cpp
+++ libgid/src/win32/gui-win32.cpp
@@ -1,5 +1,70 @@
 #include <gglobal.h>
 #include <gevent.h>
+#include <map>
+#include "resource.h"
+
+class AlertDialog
+{
+public:
+  string title,message;
+  string cancelButton,button1,button2;
+  void *udata;
+  gevent_Callback callback;
+}
+
+static std::map<g_id, AlertDialog> map_;
+static g_id mygid;
+
+BOOL CALLBACK AlertDialogProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+
+  if (iMsg==WM_INITDIALOG){
+
+    SetWindowText(hwnd,map_[mygid].title.c_str());
+    SetDlgItemText(hwnd,ID_MESSAGE,map_[mygid].message.c_str());
+
+    SetDlgItemText(hwnd,ID_BUTTON1,map_[mygid].button1.c_str());
+    SetDlgItemText(hwnd,ID_BUTTON2,map_[mygid].button2.c_str());
+    SetDlgItemText(hwnd,ID_BUTTON3,map_[mygid].button3.c_str());
+
+    return TRUE;
+  }
+  else if (iMsg==WM_COMMAND){
+    if (LOWORD(wParam)==ID_BUTTON1){
+      size_t size = sizeof(gui_AlertDialogCompleteEvent) + map_[mygid].button1.length() + 1;
+      gui_AlertDialogCompleteEvent *event = (gui_AlertDialogCompleteEvent*)malloc(size);
+
+      event->gid=mygid;
+      event->buttonText=(char*)event+sizeof(gui_AlertDialogCompleteEvent);
+      event->buttonIndex=1;
+      strcpy((char*)event->buttonText, map_[gid].button1.c_str());
+    }
+    else if (LOWORD(wParam)==ID_BUTTON2){
+      size_t size = sizeof(gui_AlertDialogCompleteEvent) + map_[mygid].button2.length() + 1;
+      gui_AlertDialogCompleteEvent *event = (gui_AlertDialogCompleteEvent*)malloc(size);
+
+      event->gid=mygid;
+      event->buttonText=(char*)event+sizeof(gui_AlertDialogCompleteEvent);
+      event->buttonIndex=2;
+      strcpy((char*)event->buttonText, map_[gid].button2.c_str());
+    }
+    else {
+      size_t size = sizeof(gui_AlertDialogCompleteEvent) + map_[mygid].cancelButton.length() + 1;
+      gui_AlertDialogCompleteEvent *event = (gui_AlertDialogCompleteEvent*)malloc(size);
+
+      event->gid=mygid;
+      event->buttonText=(char*)event+sizeof(gui_AlertDialogCompleteEvent);
+      event->buttonIndex=0;
+      strcpy((char*)event->buttonText, map_[gid].cancelButton.c_str());
+    }
+
+    gevent_EnqueueEvent(mygid, map_[mygid].callback, GUI_ALERT_DIALOG_COMPLETE_EVENT, 
+			event, 1, map_[mygid].udata);
+    return TRUE;
+  }
+  else
+    return FALSE;
+}
 
 extern "C" {
 
@@ -22,8 +87,22 @@ G_API g_id gui_createAlertDialog(const char *title,
                                  gevent_Callback callback,
                                  void *udata)
 {
-  //    return s_manager->createAlertDialog(title, message, cancelButton, button1, button2, callback, udata);
-	return 0;
+
+  g_id gid = g_NextId();
+
+  AlertDialog alertDialog;
+
+  alertDialog.title=title;
+  alertDialog.message=message;
+  alertDialog.cancelButton;
+  alertDialog.button1=button1;
+  alertDialog.button2=button2;
+  alertDialog.callback=callback;
+  alertDialog.udata=udata;
+
+  map_[gid]=alertDialog;
+  
+  return gid;
 }
 
 G_API g_id gui_createTextInputDialog(const char *title,
@@ -41,7 +120,17 @@ G_API g_id gui_createTextInputDialog(const char *title,
 
 G_API void gui_show(g_id gid)
 {
-  //    s_manager->show(gid);
+
+  std::map<g_id, AlertDialog>::iterator iter = map_.find(gid);
+
+  if (iter == map_.end())
+    throw std::runtime_error("invalid gid");
+  
+  iter->second.dialog->ShowAsync();
+
+  DialogBox(hInst,MAKEINTRESOURCE(ID_ALERTDIALOG),hwnd,AlertDialogProc);
+
+  mygid = gid;
 }
 
 G_API void gui_hide(g_id gid)
--- scripts/buildwinrt.bat
+++ scripts/buildwinrt.bat
@@ -1,26 +1,23 @@
-REM consider /clp:Verbosity=minimal
-
 cd ..\plugins\luasocket\source\winrt\luasocket
-msbuild luasocket.sln /p:Configuration=Debug   /p:Platform=Win32 /clp:ErrorsOnly
-msbuild luasocket.sln /p:Configuration=Release /p:Platform=Win32 /clp:ErrorsOnly
-msbuild luasocket.sln /p:Configuration=Debug   /p:Platform=ARM   /clp:ErrorsOnly
-msbuild luasocket.sln /p:Configuration=Release /p:Platform=ARM   /clp:ErrorsOnly
+msbuild luasocket.sln /p:Configuration=Debug   /p:Platform=Win32 /clp:Verbosity=minimal
+msbuild luasocket.sln /p:Configuration=Release /p:Platform=Win32 /clp:Verbosity=minimal
+msbuild luasocket.sln /p:Configuration=Debug   /p:Platform=ARM   /clp:Verbosity=minimal
+msbuild luasocket.sln /p:Configuration=Release /p:Platform=ARM   /clp:Verbosity=minimal
 
 cd ..\..\..\..\..\2dsg\gfxbackends\dx11
 
 call compileshaders.bat
 
 cd ..\..\..\winrt
-msbuild gideros.sln /p:Configuration=Debug   /p:Platform=Win32 /clp:ErrorsOnly
-msbuild gideros.sln /p:Configuration=Release /p:Platform=Win32 /clp:ErrorsOnly
-msbuild gideros.sln /p:Configuration=Debug   /p:Platform=ARM   /clp:ErrorsOnly
-msbuild gideros.sln /p:Configuration=Release /p:Platform=ARM   /clp:ErrorsOnly
+msbuild gideros.sln /p:Configuration=Debug   /p:Platform=Win32 /clp:Verbosity=minimal
+msbuild gideros.sln /p:Configuration=Release /p:Platform=Win32 /clp:Verbosity=minimal
+msbuild gideros.sln /p:Configuration=Debug   /p:Platform=ARM   /clp:Verbosity=minimal
+msbuild gideros.sln /p:Configuration=Release /p:Platform=ARM   /clp:Verbosity=minimal
 
-cd ..\winrt_example
-REM msbuild giderosgame.sln /t:publish /p:Configuration=Debug   /p:Platform=Win32 /clp:ErrorsOnly /p:AppxBundle=Always
-REM msbuild giderosgame.sln /t:publish /p:Configuration=Debug   /p:Platform=ARM   /clp:ErrorsOnly /p:AppxBundle=Always
+cd ..\winrt_example\giderosgame\giderosgame.Windows
+msbuild giderosgame.Windows.vcxproj /t:publish /p:Configuration=Release /p:Platform=Win32 /clp:Verbosity=minimal /p:AppxBundle=Always
 
-msbuild giderosgame.sln /t:publish /p:Configuration=Release /p:Platform=Win32 /clp:ErrorsOnly /p:AppxBundle=Always
-msbuild giderosgame.sln /t:publish /p:Configuration=Release /p:Platform=ARM   /clp:ErrorsOnly /p:AppxBundle=Always
+cd ..\giderosgame.WindowsPhone
+msbuild giderosgame.WindowsPhone.vxcproj /t:publish /p:Configuration=Release /p:Platform=ARM /clp:Verbosity=minimal /p:AppxBundle=Always
 
-cd ..\scripts
+cd ..\..\..\scripts
