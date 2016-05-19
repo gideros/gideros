8e1d7cc1cd15585d34f97dbf700f5f46911ff9c0
Nicolas Bouquet <hgy01@hieroglyphe.net>
2016-05-19 16:03:55 +0200

    Fix blur shader syntax for GL ES

--- samplecode/Shaders/Blur/fShader.glsl
+++ samplecode/Shaders/Blur/fShader.glsl
@@ -7,13 +7,13 @@ varying mediump vec2 fTexCoord;
 void main() {
  lowp vec4 frag=vec4(0,0,0,0);
  int ext=2*fRad+1;	
- mediump vec2 tc=fTexCoord-fTexelSize.xy*fRad;
+ mediump vec2 tc=fTexCoord-fTexelSize.xy*float(fRad);
  for (int v=0;v<ext;v++)	
  {
 	frag=frag+texture2D(fTexture, tc);
     tc+=fTexelSize.xy;
  }
- frag=frag/ext;
+ frag=frag/float(ext);
  if (frag.a==0.0) discard;
  gl_FragColor = frag;
 }
