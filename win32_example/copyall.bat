ccb1ade50c1aaac5fa292690578f2d3f6c7acb1d
John Blackburn <john.blackburn75@gmail.com>
2016-06-18 00:37:24 +0100

    [win32] grad dlls

--- win32_example/copyall.bat
+++ win32_example/copyall.bat
@@ -1,6 +1,10 @@
 copy ..\libgid\external\glew-1.10.0\lib\mingw48_32\glew32.dll .
 copy ..\libgid\external\openal-soft-1.13\build\mingw48_32\OpenAL32.dll .
 copy "..\libgid\external\pthreads-w32-2-9-1-release\Pre-built.2\dll\x86\pthreadGC2.dll" .
+copy ..\libgid\external\curl-7.40.0-devel-mingw32\bin\libcurl.dll .
+copy ..\libgid\external\curl-7.40.0-devel-mingw32\bin\libidn-11.dll .
+copy ..\libgid\external\curl-7.40.0-devel-mingw32\bin\libeay32.dll .
+copy ..\libgid\external\curl-7.40.0-devel-mingw32\bin\ssleay32.dll .
 
 :: Depends on location of mingw
 copy c:\mingw\bin\libgcc_s_dw2-1.dll .
