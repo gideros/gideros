67f497ae728b3f46150c14016bf3b3ef4443ed0d
John Blackburn <john.blackburn75@gmail.com>
2016-07-15 00:30:09 +0100

    [scripts] update to Qt 5.6

---
+++ scripts/mycopyqt56.bat
@@ -0,0 +1,36 @@
+copy C:\Qt\5.6\mingw49_32\bin\icudt54.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\icuin54.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\icuuc54.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\libgcc_s_dw2-1.dll ..\..\release
+copy "C:\Qt\5.6\mingw49_32\bin\libstdc++-6.dll" ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\libwinpthread-1.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Core.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Gui.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Network.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5OpenGL.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5PrintSupport.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Widgets.dll ..\..\release
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Xml.dll ..\..\release
+
+mkdir ..\..\release\imageformats
+copy C:\Qt\5.6\mingw49_32\plugins\imageformats\qjpeg.dll ..\..\release\imageformats
+
+mkdir ..\..\release\platforms
+copy C:\Qt\5.6\mingw49_32\plugins\platforms\qminimal.dll ..\..\release\platforms
+copy C:\Qt\5.6\mingw49_32\plugins\platforms\qoffscreen.dll ..\..\release\platforms
+copy C:\Qt\5.6\mingw49_32\plugins\platforms\qwindows.dll ..\..\release\platforms
+
+copy C:\qscintilla\QScintilla-gpl-2.8.4\Qt4Qt5\release\qscintilla2.dll ..\..\release
+
+copy ..\libgid\external\glew-1.10.0\lib\mingw48_32\glew32.dll ..\..\release
+copy ..\libgid\external\openal-soft-1.13\build\mingw48_32\OpenAL32.dll ..\..\release
+
+copy C:\Qt\5.6\mingw49_32\bin\icudt54.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\icuin54.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\icuuc54.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\libgcc_s_dw2-1.dll ..\..\release\Tools
+copy "C:\Qt\5.6\mingw49_32\bin\libstdc++-6.dll" ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\libwinpthread-1.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Core.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Network.dll ..\..\release\Tools
+copy C:\Qt\5.6\mingw49_32\bin\Qt5Xml.dll ..\..\release\Tools
