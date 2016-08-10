e9fe311c3c72f39ab541095cbc61c0d46afb312b
Nikolay Yevstakhov <n1cke@mail.ru>
2016-08-10 15:17:41 +0300

    short macro: fix for numbers at the end of stream

--- lua514u/src/llex.c
+++ lua514u/src/llex.c
@@ -538,7 +538,7 @@ static int llex (LexState *ls, SemInfo *seminfo) {
           case '-': case '0': case '1': case '2': case '3': case '4':
           case '.': case '5': case '6': case '7': case '8': case '9':
           {
-            while (!isspace(ls->current)) save_and_next(ls);
+            while (!isspace(ls->current) && ls->current != EOZ) save_and_next(ls);
             break;
           }
           case '`': case '~': case '!': case '#': case '$': case '%': case '^':
