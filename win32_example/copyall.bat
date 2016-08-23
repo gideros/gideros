24e8cc198fe58c467715769a6ef3c7f02b68e1d6
John Blackburn <john.blackburn75@gmail.com>
2016-08-23 12:21:19 +0100

    [win32] copy missing DLL

--- win32_example/copyall.bat
+++ win32_example/copyall.bat
@@ -9,3 +9,4 @@ copy ..\libgid\external\curl-7.40.0-devel-mingw32\bin\ssleay32.dll .
 :: Depends on location of mingw
 copy c:\mingw\bin\libgcc_s_dw2-1.dll .
 copy "c:\mingw\bin\libstdc++-6.dll" .
+copy c:\mingw\bin\zlib1.dll .
