6763ea76897f3fa1fab3568b895901becf06c469
hgy29 <hgy01@hieroglyphe.net>
2016-10-02 20:10:48 +0200

    [sample] Update horse example

--- samplecode/Graphics3D/3D-Horse/horse.lua
+++
@@ -1,8 +0,0 @@
-function buildHorse()
-local horse=loadObj("horse","LD_HorseRtime02.obj")
-horse:setScale(40,-40,40)
-horse:setRotationX(0)
-horse:setRotationY(90)
-horse:setPosition(300,300,-400)
-return horse
-end
