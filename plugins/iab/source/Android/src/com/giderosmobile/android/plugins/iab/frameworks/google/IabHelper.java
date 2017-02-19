ed104f02e26541b632ba2303151218d05dbc6325
hgy29 <hgy01@hieroglyphe.net>
2017-02-19 18:44:36 +0100

    [plugin/iab] Update/Fix Helper [html5] Remove unneeded files

--- plugins/iab/source/Android/src/com/giderosmobile/android/plugins/iab/frameworks/google/IabHelper.java
+++ plugins/iab/source/Android/src/com/giderosmobile/android/plugins/iab/frameworks/google/IabHelper.java
@@ -939,60 +939,70 @@ public class IabHelper {
         logDebug("Package name: " + mContext.getPackageName());
         boolean verificationFailed = false;
         String continueToken = null;
+		if (mService != null) {
+			if (mContext != null) {
+				do {
+					logDebug("Calling getPurchases with continuation token: " + continueToken);
+					Bundle ownedItems = mService.getPurchases(3, mContext.getPackageName(),
+							itemType, continueToken);
 
-        do {
-            logDebug("Calling getPurchases with continuation token: " + continueToken);
-            Bundle ownedItems = mService.getPurchases(3, mContext.getPackageName(),
-                    itemType, continueToken);
+					int response = getResponseCodeFromBundle(ownedItems);
+					logDebug("Owned items response: " + String.valueOf(response));
+					if (response != BILLING_RESPONSE_RESULT_OK) {
+						logDebug("getPurchases() failed: " + getResponseDesc(response));
+						return response;
+					}
+					if (!ownedItems.containsKey(RESPONSE_INAPP_ITEM_LIST)
+							|| !ownedItems.containsKey(RESPONSE_INAPP_PURCHASE_DATA_LIST)
+							|| !ownedItems.containsKey(RESPONSE_INAPP_SIGNATURE_LIST)) {
+						logError("Bundle returned from getPurchases() doesn't contain required fields.");
+						return IABHELPER_BAD_RESPONSE;
+					}
 
-            int response = getResponseCodeFromBundle(ownedItems);
-            logDebug("Owned items response: " + String.valueOf(response));
-            if (response != BILLING_RESPONSE_RESULT_OK) {
-                logDebug("getPurchases() failed: " + getResponseDesc(response));
-                return response;
-            }
-            if (!ownedItems.containsKey(RESPONSE_INAPP_ITEM_LIST)
-                    || !ownedItems.containsKey(RESPONSE_INAPP_PURCHASE_DATA_LIST)
-                    || !ownedItems.containsKey(RESPONSE_INAPP_SIGNATURE_LIST)) {
-                logError("Bundle returned from getPurchases() doesn't contain required fields.");
-                return IABHELPER_BAD_RESPONSE;
-            }
+					ArrayList<String> ownedSkus = ownedItems.getStringArrayList(
+							RESPONSE_INAPP_ITEM_LIST);
+					ArrayList<String> purchaseDataList = ownedItems.getStringArrayList(
+							RESPONSE_INAPP_PURCHASE_DATA_LIST);
+					ArrayList<String> signatureList = ownedItems.getStringArrayList(
+							RESPONSE_INAPP_SIGNATURE_LIST);
 
-            ArrayList<String> ownedSkus = ownedItems.getStringArrayList(
-                    RESPONSE_INAPP_ITEM_LIST);
-            ArrayList<String> purchaseDataList = ownedItems.getStringArrayList(
-                    RESPONSE_INAPP_PURCHASE_DATA_LIST);
-            ArrayList<String> signatureList = ownedItems.getStringArrayList(
-                    RESPONSE_INAPP_SIGNATURE_LIST);
+					for (int i = 0; i < purchaseDataList.size(); ++i) {
+						String purchaseData = purchaseDataList.get(i);
+						String signature = signatureList.get(i);
+						String sku = ownedSkus.get(i);
+						if (Security.verifyPurchase(mSignatureBase64, purchaseData, signature)) {
+							logDebug("Sku is owned: " + sku);
+							Purchase purchase = new Purchase(itemType, purchaseData, signature);
 
-            for (int i = 0; i < purchaseDataList.size(); ++i) {
-                String purchaseData = purchaseDataList.get(i);
-                String signature = signatureList.get(i);
-                String sku = ownedSkus.get(i);
-                if (Security.verifyPurchase(mSignatureBase64, purchaseData, signature)) {
-                    logDebug("Sku is owned: " + sku);
-                    Purchase purchase = new Purchase(itemType, purchaseData, signature);
+							if (TextUtils.isEmpty(purchase.getToken())) {
+								logWarn("BUG: empty/null token!");
+								logDebug("Purchase data: " + purchaseData);
+							}
 
-                    if (TextUtils.isEmpty(purchase.getToken())) {
-                        logWarn("BUG: empty/null token!");
-                        logDebug("Purchase data: " + purchaseData);
-                    }
+							// Record ownership and token
+							inv.addPurchase(purchase);
+						}
+						else {
+							logWarn("Purchase signature verification **FAILED**. Not adding item.");
+							logDebug("   Purchase data: " + purchaseData);
+							logDebug("   Signature: " + signature);
+							verificationFailed = true;
+						}
+					}
 
-                    // Record ownership and token
-                    inv.addPurchase(purchase);
-                }
-                else {
-                    logWarn("Purchase signature verification **FAILED**. Not adding item.");
-                    logDebug("   Purchase data: " + purchaseData);
-                    logDebug("   Signature: " + signature);
-                    verificationFailed = true;
-                }
-            }
-
-            continueToken = ownedItems.getString(INAPP_CONTINUATION_TOKEN);
-            logDebug("Continuation token: " + continueToken);
-        } while (!TextUtils.isEmpty(continueToken));
-
+					continueToken = ownedItems.getString(INAPP_CONTINUATION_TOKEN);
+					logDebug("Continuation token: " + continueToken);
+				} while (!TextUtils.isEmpty(continueToken));
+			}
+			else {
+				logWarn("queryPurchaes **FAILED**. mContext is null.");
+				verificationFailed = true;
+			}
+		}
+		else {
+			logWarn("queryPurchaes **FAILED**. mService is null.");
+			verificationFailed = true;
+		}
         return verificationFailed ? IABHELPER_VERIFICATION_FAILED : BILLING_RESPONSE_RESULT_OK;
     }
 
