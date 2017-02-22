37b215c6b668140823e254a59f6167f4e2d4f224
alexzheng <yizheng@YIs-MacBook-Pro.local>
2017-02-22 11:18:55 +0800

    [plugin gmedia]Fix orientation issue, and remove deprecated apis
    Fix orientation issue, and remove deprecated apis

--- plugins/gmedia/source/iOS/Plugins/Media/GMediaClass.mm
+++ plugins/gmedia/source/iOS/Plugins/Media/GMediaClass.mm
@@ -12,6 +12,100 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+
+
+@interface UIImage (fixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
+
+@implementation UIImage (fixOrientation)
+
+- (UIImage *)fixOrientation {
+    
+    // No-op if the orientation is already correct
+    if (self.imageOrientation == UIImageOrientationUp) return self;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, self.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
+                                             CGImageGetBitsPerComponent(self.CGImage), 0,
+                                             CGImageGetColorSpace(self.CGImage),
+                                             CGImageGetBitmapInfo(self.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (self.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
+@end
+
+
+
 @implementation GMediaClass
 
 -(id)init{
@@ -38,7 +132,7 @@
         [imagePicker setSourceType:UIImagePickerControllerSourceTypeCamera];
     }
     [imagePicker setDelegate:self];
-    [g_getRootViewController() presentModalViewController:imagePicker animated:YES];
+    [g_getRootViewController() presentViewController:imagePicker animated:YES completion:nil];
 }
 
 -(void)takeScreenshot{
@@ -84,7 +178,7 @@
                                animated:YES];
     } else {
         // iPhone Code:
-        [g_getRootViewController() presentModalViewController:imagePicker animated:YES];
+        [g_getRootViewController() presentViewController:imagePicker animated:YES completion:nil];
     }
 }
 
@@ -106,12 +200,14 @@
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
         [self removePopover];
     }
-    [picker dismissModalViewControllerAnimated:YES];
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
     [picker release];
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
     UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
     if(image != nil){
-        NSString *path = [self saveImage:image];
+        UIImage *fixImage = [image fixOrientation];
+        NSString *path = [self saveImage:fixImage];
         gmedia_onMediaReceive([path UTF8String]);
     }
     else
@@ -124,7 +220,8 @@
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
         [self removePopover];
     }
-    [picker dismissModalViewControllerAnimated:YES];
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
     [picker release];
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
     gmedia_onMediaCanceled();
