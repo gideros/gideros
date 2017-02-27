bd508b3fa6c3180d22bd8fba976cec75db99ffda
Nicolas Bouquet <hgy01@hieroglyphe.net>
2017-02-27 15:43:11 +0100

    [lua] Fix llex.c compile error

--- lua/src/llex.c
+++ lua/src/llex.c
@@ -664,7 +664,7 @@ static int llex (LexState *ls, SemInfo *seminfo) {
                     fseek (f, 0, SEEK_END);
                     length = ftell (f);
                     fseek (f, 0, SEEK_SET);
-                    buffer = malloc (length + 1);
+                    buffer = (char *) malloc (length + 1);
                     if (buffer)
                     {
                       fread (buffer, 1, length, f);
