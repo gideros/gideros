7e2076dd6cffd1c7f40e74a99c6f815d913a8fd2
Nicolas Bouquet <hgy01@hieroglyphe.net>
2017-03-06 14:41:27 +0100

    [mk] Sign OSX installer

--- scripts/GidQtMac.mk
+++ scripts/GidQtMac.mk
@@ -175,5 +175,8 @@ bundle.installer: bundle
 	mkdir  -p $(ROOT)/ROOTMAC/Applications
 	mv $(RELEASE).Final $(ROOT)/ROOTMAC/Applications/Gideros\ Studio
 	rm -f $(ROOT)/Gideros.pkg
-	pkgbuild --root $(ROOT)/ROOTMAC --identifier com.giderosmobile.gideros --component-plist $(ROOT)/Release/pkg.plist $(ROOT)/Gideros.pkg
+	pkgbuild --root $(ROOT)/ROOTMAC --identifier com.giderosmobile.gideros --version $(GIDEROS_VERSION) --component-plist $(ROOT)/Release/pkg.plist $(ROOT)/Gideros-App.pkg
+	productbuild --distribution Release/GiderosDist.plist --package-path $(ROOT)/Gideros-App.pkg --sign $(OSX_SIGNING_IDENTITY) $(ROOT)/Gideros.pkg
+	
+	
 	rm -rf $(ROOT)/ROOTMAC
