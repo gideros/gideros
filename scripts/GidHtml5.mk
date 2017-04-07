c79b319d2329c2fe126ff5fe18a39738f77fe656
Nicolas BOUQUET <hgy01@hieroglyphe.net>
2017-04-07 07:50:04 +0200

    [html5] Fix tools compilation on MacOS

--- scripts/GidHtml5.mk
+++ scripts/GidHtml5.mk
@@ -34,12 +34,12 @@ CRUNCHME_SRCS+=$(addprefix src/zlib/,adler32 compress crc32 deflate inftrees tre
 CRUNCHME_SRCS+=$(addprefix src/,crunchme png)
 
 html5.crunchme: $(addprefix emscripten/crunchme-0.4/,$(addsuffix .co,$(CRUNCHME_SRCS)))
-	$(CXX)  -o $(ROOT)/ui/Tools/crunchme $^
+	g++  -o $(ROOT)/ui/Tools/crunchme $^
 	
 %.co: %.cpp
-	$(CXX) -c $< -Iemscripten/crunchme-0.4/src/liblzg/include -o $@
+	g++ -c $< -Iemscripten/crunchme-0.4/src/liblzg/include -o $@
 
 %.co: %.c
-	$(CC) -c $< -o $@
+	gcc -c $< -o $@
 
 	
