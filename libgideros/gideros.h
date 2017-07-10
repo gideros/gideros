aac090866369c34aacfcfb19703d8d4d2c9c7e5d
Nicolas BOUQUET <hgy01@hieroglyphe.net>
2017-07-10 11:22:48 +0200

    [android] Fix export script [version] Bump to 2017.6.1

--- libgideros/gideros.h
+++ libgideros/gideros.h
@@ -23,7 +23,7 @@
 #endif
 
 #ifndef GIDEROS_VERSION
-#define GIDEROS_VERSION "2017.6"
+#define GIDEROS_VERSION "2017.6.1"
 #endif
 
 #endif
--- ui/Tools/export_android.lua
+++ ui/Tools/export_android.lua
@@ -3,36 +3,36 @@ Tools to simplify android exports
 ]]
 
 AndroidProject={
-	permissions={}
+  permissions={}
 }
 
 local MAX_SDK=1000
 
 function AndroidProject.usePermission(name,maxSdk)
-	local msdk=maxSdk or MAX_SDK
-	local csdk=AndroidProject.permissions[name]
-	if csdk==nil or csdk<msdk then
-		AndroidProject.permissions[name]=maxSdk
-	end
+  local msdk=maxSdk or MAX_SDK
+  local csdk=AndroidProject.permissions[name]
+  if csdk==nil or csdk<msdk then
+    AndroidProject.permissions[name]=msdk
+  end
 end
 
 local function apply()
-	local perms=""
-	for n,s in pairs(AndroidProject.permissions) do
-		local e=""
-		if s<MAX_SDK then
-			e=([[android:maxSdkVersion="%d" ]]):format(s)
-		end
-		perms=perms..(([[
-		<uses-permission android:name="%s" %s/>]]):format(n,e))
-	end
- 	Export.callXml(([[<template name="Manifest" path="">
-			<replacelist wildcards="AndroidManifest.xml">
-				<append>
-				<orig>]].."<![CDATA[<!-- TAG:MANIFEST-EXTRA -->]]></orig><by><![CDATA[%s]]></by>"..
-				[[</append>
-	       	</replacelist>
- 	</template>]]):format(perms))
+  local perms=""
+  for n,s in pairs(AndroidProject.permissions) do
+    local e=""
+    if s<MAX_SDK then
+      e=([[android:maxSdkVersion="%d" ]]):format(s)
+    end
+    perms=perms..(([[
+    <uses-permission android:name="%s" %s/>]]):format(n,e))
+  end
+  Export.callXml(([[<template name="AndroidPermissionsManifest" path="">
+      <replacelist wildcards="AndroidManifest.xml">
+        <append>
+        <orig>]].."<![CDATA[<!-- TAG:MANIFEST-EXTRA -->]]></orig><by><![CDATA[%s]]></by>"..
+        [[</append>
+          </replacelist>
+  </template>]]):format(perms))
 end
 
 Export.registerPreFinish(apply)
