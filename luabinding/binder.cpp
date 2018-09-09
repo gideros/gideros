0eabb1de21be7d525b9717267476137e0f0fc63a
Paul Reilly <paul@cartesian.tech>
2018-09-09 11:53:13 +0100

    [binder]  tidy

--- luabinding/binder.cpp
+++ luabinding/binder.cpp
@@ -5,40 +5,42 @@
 
 void Binder::disableTypeChecking()
 {
-	g_disableTypeChecking();
+    g_disableTypeChecking();
 }
 
 void Binder::enableTypeChecking()
 {
-	g_enableTypeChecking();
+    g_enableTypeChecking();
 }
 
 void Binder::createClass(const char* classname,
-						 const char* basename,
-						 int (*constructor) (lua_State*),
-						 int (*destructor) (lua_State*),
-						 const luaL_reg* functionlist)
+                         const char* basename,
+                         int (*constructor) (lua_State*),
+                         int (*destructor) (lua_State*),
+                         const luaL_reg* functionlist)
 {
-	g_createClass(L, classname, basename, constructor, destructor, functionlist);
+    g_createClass(L, classname, basename, constructor, destructor, functionlist);
+}
+
 }
 
 void Binder::pushInstance(const char* classname, void* ptr)
 {
-	g_pushInstance(L, classname, ptr);
+    g_pushInstance(L, classname, ptr);
 }
 
 bool Binder::isInstanceOf(const char* classname, int index) const
 {
-	return g_isInstanceOf(L, classname, index) != 0;
+    return g_isInstanceOf(L, classname, index) != 0;
 }
 
 void* Binder::getInstance(const char* classname, int index) const
 {
-	return g_getInstance(L, classname, index);
+    return g_getInstance(L, classname, index);
 }
 
 void Binder::setInstance(int index, void* ptr)
 {
-	g_setInstance(L, index, ptr);
+    g_setInstance(L, index, ptr);
 }
 
