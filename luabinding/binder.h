40e8271fb7a2fc1faf38b13afa81369ef5b682ab
Paul Reilly <paul@cartesian.tech>
2018-09-09 11:53:13 +0100

    [binder]  tidy

--- luabinding/binder.h
+++ luabinding/binder.h
@@ -1,39 +1,33 @@
-#ifndef BINDER_H
-#define BINDER_H
+#pragma once
 
-//extern "C"
-//{
-//#include "lua.h"
-//#include "lauxlib.h"
-//#include "lualib.h"
-//}
 #include "lua.hpp"
+#include <string>
+#include <vector>
 
 class Binder
 {
 public:
-	explicit Binder(lua_State* L) : L(L)
-	{
-	}
-	
-	void createClass(const char* classname,
-					 const char* basename,
-					 int (*constructor) (lua_State*),
-					 int (*destructor) (lua_State*),
-					 const luaL_reg* functionlist);
+    explicit Binder(lua_State* L) : L(L)
+    {
+    }
 
-	void pushInstance(const char* classname, void* ptr);
+    void createClass(const char* classname,
+                     const char* basename,
+                     int (*constructor) (lua_State*),
+                     int (*destructor) (lua_State*),
+                     const luaL_reg* functionlist);
 
-	void* getInstance(const char* classname, int index = 1) const;
 
-	void setInstance(int index, void* ptr);
+    void pushInstance(const char* classname, void* ptr);
 
-	bool isInstanceOf(const char* classname, int index) const;
-	
-	static void disableTypeChecking();
-	static void enableTypeChecking();
+    void* getInstance(const char* classname, int index = 1) const;
 
-	lua_State* L;
+    void setInstance(int index, void* ptr);
+
+    bool isInstanceOf(const char* classname, int index) const;
+
+    static void disableTypeChecking();
+    static void enableTypeChecking();
+
+    lua_State* L;
 };
-
-#endif
