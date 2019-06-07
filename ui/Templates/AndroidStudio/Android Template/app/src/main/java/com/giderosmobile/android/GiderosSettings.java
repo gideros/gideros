381c57d45615107946f275f23495fa0fbae0ce44
Nicolas BOUQUET <hgy01@hieroglyphe.net>
2019-06-07 13:59:49 +0200

    [android] Improve config chooser (select alpha if possible) [android] Handle AAB and allow transparent canvas

--- ui/Templates/AndroidStudio/Android Template/app/src/main/java/com/giderosmobile/android/GiderosSettings.java
+++ ui/Templates/AndroidStudio/Android Template/app/src/main/java/com/giderosmobile/android/GiderosSettings.java
@@ -4,4 +4,5 @@ import android.view.View;
 public class GiderosSettings {
 	public static boolean backgroundLocation=false;
 	public static View mainView=null;
+	public static boolean translucentCanvas=false;
 };
