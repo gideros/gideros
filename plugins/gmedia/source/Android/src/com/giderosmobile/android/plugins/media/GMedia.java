ef3e86cf35ad87ec9aa19f6b446a38598bfed7d3
mokalux <mouradlux@gmail.com>
2019-10-03 16:46:47 +0200

    Update GMedia.java
    added:
    - ask for permission CAMERA
    - ask for permission R/W external storage
    - take photo
    - play video
    - take screenshot
    - save to SD/Pictures/Gideros folder (WE CAN CHANGE THE FOLDER NAME)
    - save format PNG or JPG

--- plugins/gmedia/source/Android/src/com/giderosmobile/android/plugins/media/GMedia.java
+++ plugins/gmedia/source/Android/src/com/giderosmobile/android/plugins/media/GMedia.java
@@ -12,6 +12,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.opengl.GLException;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
@@ -21,10 +22,12 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.nio.IntBuffer;
 import java.text.SimpleDateFormat;
@@ -33,12 +36,16 @@ import java.util.Date;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GMedia {
+	private static final int PERMISSION_ACCESS_CAMERA = 1;
+	private static final int REQUEST_TAKE_PICTURE = 2;
+	private static final int REQUEST_SELECT_PICTURE = 3;
+	private static final int PERMISSION_READ_FOR_SELECT_PICTURE = 4;
+	private static final int PERMISSION_WRITE_FOR_SCREENSHOT = 5;
+	private static final int PERMISSION_WRITE_FOR_SAVE_PICTURE = 6;
 	private static WeakReference<Activity> sActivity;
 	private static long sData = 0;
 	private static boolean takeScreenshot = false;
-	private static final int REQUEST_TAKE_PHOTO = 2;
-	private static final int SELECT_PICTURE = 3;
-	private static final int PERMISSION_WRITE_EXTERNAL_STORAGE = 114; // API 23+
+	private static String xpath = "";
 	private static String returnPath = "";
 	private static int screenWidth;
 	private static int screenHeight;
@@ -55,106 +62,43 @@ public class GMedia {
 		sData = data;
 	}
 
-	public static boolean isCameraAvailable() {
+	// gideros functions
+	public static boolean isCameraAvailable() { // OK
 		PackageManager pm = sActivity.get().getPackageManager();
 		return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
 	}
 
-	public static void takeScreenshot() {
-		takeScreenshot = true;
-	}
-
-	public static void takePicture() {
-		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		if (takePictureIntent.resolveActivity(sActivity.get().getPackageManager()) != null) {
-			File photoFile = new File(createImageFile());
-
-			if (photoFile != null) {
-				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-						Uri.fromFile(photoFile));
-				sActivity.get().startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-			}
+	public static void takePicture() { // OK PHOTO BUT THUMBNAIL!
+		if (Build.VERSION.SDK_INT >= 23 && !isPermissionCamera()) {
+			requestPermissionCamera();
+		} else {
+			takePictureFunction();
 		}
 	}
 
-	public static void getPicture() {
-		Intent i = new Intent(
-				Intent.ACTION_PICK,
-				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-		sActivity.get().startActivityForResult(i, SELECT_PICTURE);
+	public static void getPicture() { // OK
+		if (Build.VERSION.SDK_INT >= 23 && !isPermissionWriteES()) {
+			requestPermissionWES(PERMISSION_READ_FOR_SELECT_PICTURE);
+		} else {
+			getPictureFunction();
+		}
 	}
 
 	public static void savePicture(String path) {
-		// request permission
-		if (Build.VERSION.SDK_INT >= 23) {
-			if (isPermissionWriteES()) {
-				// permission OK can save image
-			} else {
-				requestPermissionWES();
-			}
-		}
-		// create the bitmap
-		Bitmap source = loadBitmap(path);
-		// save to public directory (SD/Pictures)
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-		String picturespath = Environment.getExternalStoragePublicDirectory(
-				Environment.DIRECTORY_PICTURES).toString() + "/GIDEROS"; // change folder name here
-		String imgname = "img_" + timeStamp + ".png"; // change image name here
-		String destination = picturespath + "/" + imgname;
-		// create directory
-		File fileimg = new File(picturespath, imgname);
-		fileimg.getParentFile().mkdirs();
-		// then save to disk
-		savePngBitmap(destination, source);
-		//let media scanner scan it
-		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-		File f = new File(destination);
-		Uri contentUri = Uri.fromFile(f);
-		mediaScanIntent.setData(contentUri);
-		sActivity.get().sendBroadcast(mediaScanIntent);
-	}
-
-	private static boolean isPermissionWriteES() {
-		int result = ContextCompat.checkSelfPermission(sActivity.get(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
-		return result == PackageManager.PERMISSION_GRANTED;
-	}
-
-	private static void requestPermissionWES() {
-		if (ActivityCompat.shouldShowRequestPermissionRationale(sActivity.get(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-			ActivityCompat.requestPermissions(sActivity.get(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-					PERMISSION_WRITE_EXTERNAL_STORAGE);
+		xpath = path;
+		if (Build.VERSION.SDK_INT >= 23 && !isPermissionWriteES()) {
+			requestPermissionWES(PERMISSION_WRITE_FOR_SAVE_PICTURE);
 		} else {
-			ActivityCompat.requestPermissions(sActivity.get(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-					PERMISSION_WRITE_EXTERNAL_STORAGE);
+			savePictureFunction();
 		}
 	}
 
-	public static void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
-			if (resultCode != Activity.RESULT_OK) {
-				onMediaCanceled();
-				return;
-			}
-			onMediaReceived();
-		} else if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
-			if (resultCode != Activity.RESULT_OK) {
-				onMediaCanceled();
-				return;
-			}
-			Uri selectedImage = data.getData();
-			String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-			Cursor cursor = sActivity.get().getContentResolver().query(selectedImage,
-					filePathColumn, null, null, null);
-			cursor.moveToFirst();
-
-			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-			String path = cursor.getString(columnIndex);
-			Bitmap source = loadBitmap(path);
-			cursor.close();
-			saveBitmap(createImageFile(), source);
-			onMediaReceived();
+	public static void takeScreenshot() { // OK
+		if (Build.VERSION.SDK_INT >= 23 && !isPermissionWriteES()) {
+			requestPermissionWES(PERMISSION_WRITE_FOR_SCREENSHOT);
+		} else {
+			showToast("in progress...");
+			takeScreenshot = true;
 		}
 	}
 
@@ -169,9 +113,164 @@ public class GMedia {
 		});
 	}
 
-	private static void onScreenshotTaken(Bitmap bitmap) {
-		saveBitmap(createImageFile(), bitmap);
+	// permissions granted functions
+	public static void takePictureFunction() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePictureIntent.resolveActivity(sActivity.get().getPackageManager()) != null) {
+			// used to crash. Passing a file:// URI across a package boundary causes a FileUriExposedException. cf: https://developer.android.com/training/camera/photobasics
+//				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+//						Uri.fromFile(photoFile));
+//				sActivity.get().startActivityForResult(takePictureIntent, REQUEST_TAKE_PICTURE);
+
+			// new version (only takes thumbnail but at least doesn't crash)
+			sActivity.get().startActivityForResult(takePictureIntent, REQUEST_TAKE_PICTURE);
+		}
+	}
+
+	public static void getPictureFunction() {
+		Intent i = new Intent(
+				Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		sActivity.get().startActivityForResult(i, REQUEST_SELECT_PICTURE);
+	}
+
+	public static void savePictureFunction() {
+		// create the bitmap
+		Bitmap source = loadBitmap(xpath);
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		// save to public directory (SD/Pictures)
+		String storageDir = Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES).toString() + "/GIDEROS"; // change name here
+		String imageFileName = "img_" + timeStamp + ".png"; // change name here
+		// create directory
+		File fileimg = new File(storageDir, imageFileName);
+		fileimg.getParentFile().mkdirs();
+		// then save to disk
+		showToast("saving...");
+		returnPath = storageDir + "/" + imageFileName;
+		savePngBitmap(returnPath, source);
+		mediaScanner(returnPath);
 		onMediaReceived();
+	}
+
+	// permissions
+	private static boolean isPermissionCamera() {
+		int result = ContextCompat.checkSelfPermission(sActivity.get(), Manifest.permission.CAMERA);
+		return result == PackageManager.PERMISSION_GRANTED;
+	}
+
+	private static void requestPermissionCamera() {
+		if (ActivityCompat.shouldShowRequestPermissionRationale(sActivity.get(), Manifest.permission.CAMERA)) {
+			ActivityCompat.requestPermissions(sActivity.get(), new String[]{Manifest.permission.CAMERA},
+					PERMISSION_ACCESS_CAMERA);
+		}
+	}
+
+	private static boolean isPermissionWriteES() {
+		int result = ContextCompat.checkSelfPermission(sActivity.get(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+		return result == PackageManager.PERMISSION_GRANTED;
+	}
+
+	private static void requestPermissionWES(int whatfor) {
+		if (ActivityCompat.shouldShowRequestPermissionRationale(sActivity.get(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+			ActivityCompat.requestPermissions(sActivity.get(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, whatfor);
+		}
+	}
+
+	// results functions
+	public static void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
+			if (resultCode != Activity.RESULT_OK) {
+				onMediaCanceled();
+				return;
+			}
+			// take photo (I couldn't get the full photo only the thumbnail!)
+			Bundle extras = data.getExtras();
+			Bitmap source = (Bitmap) extras.get("data"); // photo thumbnail
+			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+			// gideros in app folder. Deleted when user uninstalls the app.
+			String storageDir = sActivity.get().getExternalFilesDir(null).toString();
+			String imageFileName = "photo_" + timeStamp + ".jpg";
+			returnPath = storageDir + "/" + imageFileName;
+			saveBitmap(returnPath, source);
+			mediaScanner(returnPath);
+			onMediaReceived();
+		} else if (requestCode == REQUEST_SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
+			if (resultCode != Activity.RESULT_OK) {
+				onMediaCanceled();
+				return;
+			}
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = {MediaStore.Images.Media.DATA};
+			Cursor cursor = sActivity.get().getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String path = cursor.getString(columnIndex);
+			Bitmap source = loadBitmap(path);
+			cursor.close();
+
+			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+			// gideros in app folder. Deleted when user uninstalls the app.
+			String storageDir = sActivity.get().getExternalFilesDir(null).toString();
+			String imageFileName = "select_" + timeStamp + ".png"; // better jpeg?
+			returnPath = storageDir + "/" + imageFileName;
+			savePngBitmap(returnPath, source);
+			mediaScanner(returnPath);
+			onMediaReceived();
+		}
+	}
+
+	public static void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		switch (requestCode) {
+			// If request is cancelled, the result arrays are empty.
+			case GMedia.PERMISSION_ACCESS_CAMERA: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted
+					GMedia.takePictureFunction();
+				} else {
+					// permission was denied
+					showToast("access camera not granted!");
+				}
+				break;
+			}
+			case GMedia.PERMISSION_READ_FOR_SELECT_PICTURE: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted
+					GMedia.getPictureFunction();
+				} else {
+					// permission was denied
+					showToast("read for select picture not granted!");
+				}
+				break;
+			}
+			case GMedia.PERMISSION_WRITE_FOR_SAVE_PICTURE: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted
+					GMedia.savePictureFunction();
+				} else {
+					// permission was denied
+					showToast("write for save picture not granted!");
+				}
+				break;
+			}
+			case GMedia.PERMISSION_WRITE_FOR_SCREENSHOT: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted
+					showToast("in progress...");
+					GMedia.takeScreenshot = true;
+				} else {
+					// permission was denied
+					showToast("write for screenshot not granted!");
+				}
+				break;
+			}
+			default: {
+				showToast("DEFAULT");
+				break;
+			}
+		}
 	}
 
 	public static void onDraw(final GL10 gl) {
@@ -187,34 +286,65 @@ public class GMedia {
 		}
 	}
 
-	private static void onMediaReceived() {
-		if (sData != 0)
-			onMediaReceived(returnPath, sData);
+	private static Bitmap createBitmapFromGLSurface(int x, int y, int w, int h, GL10 gl)
+			throws OutOfMemoryError {
+		int[] bitmapBuffer = new int[w * h];
+		int[] bitmapSource = new int[w * h];
+		IntBuffer intBuffer = IntBuffer.wrap(bitmapBuffer);
+		intBuffer.position(0);
+
+		try {
+			gl.glReadPixels(x, y, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, intBuffer);
+			int offset1, offset2;
+			for (int i = 0; i < h; i++) {
+				offset1 = i * w;
+				offset2 = (h - i - 1) * w;
+				for (int j = 0; j < w; j++) {
+					int texturePixel = bitmapBuffer[offset1 + j];
+					int blue = (texturePixel >> 16) & 0xff;
+					int red = (texturePixel << 16) & 0x00ff0000;
+					int pixel = (texturePixel & 0xff00ff00) | red | blue;
+					bitmapSource[offset2 + j] = pixel;
+				}
+			}
+		} catch (GLException e) {
+			return null;
+		}
+
+		return Bitmap.createBitmap(bitmapSource, w, h, Bitmap.Config.ARGB_8888);
 	}
 
-	private static void onMediaCanceled() {
-		if (sData != 0)
-			onMediaCanceled(sData);
-	}
-
-	static void onPlaybackComplete() {
-		if (sData != 0)
-			onMediaCompleted(sData);
-	}
-
-	static native void onMediaReceived(String path, long data);
-
-	static native void onMediaCanceled(long data);
-
-	static native void onMediaCompleted(long data);
-
-	private static String createImageFile() {
-		// Create an image file name
+	private static void onScreenshotTaken(Bitmap bitmap) {
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-		String imageFileName = "img_" + timeStamp + ".jpg";
-		String storageDir = sActivity.get().getExternalFilesDir(null).toString();
+		String storageDir = Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES).toString();
+		String imageFileName = "capt_" + timeStamp + ".jpg"; // can change name here!
 		returnPath = storageDir + "/" + imageFileName;
-		return returnPath;
+		saveBitmap(returnPath, bitmap);
+		mediaScanner(returnPath);
+		onMediaReceived();
+	}
+
+	private static void mediaScanner(String destination) {
+		//let media scanner scan it
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		File f = new File(destination);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		sActivity.get().sendBroadcast(mediaScanIntent);
+	}
+
+	private static void showToast(final String msg) {
+		Thread thread = new Thread() {
+			public void run() {
+				sActivity.get().runOnUiThread(new Runnable() {
+					public void run() {
+						Toast.makeText(sActivity.get().getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+					}
+				});
+			}
+		};
+		thread.start();
 	}
 
 	private static Bitmap loadBitmap(String path) {
@@ -243,33 +373,27 @@ public class GMedia {
 		}
 	}
 
-	private static Bitmap createBitmapFromGLSurface(int x, int y, int w, int h, GL10 gl)
-			throws OutOfMemoryError {
-		int bitmapBuffer[] = new int[w * h];
-		int bitmapSource[] = new int[w * h];
-		IntBuffer intBuffer = IntBuffer.wrap(bitmapBuffer);
-		intBuffer.position(0);
-
-		try {
-			gl.glReadPixels(x, y, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, intBuffer);
-			int offset1, offset2;
-			for (int i = 0; i < h; i++) {
-				offset1 = i * w;
-				offset2 = (h - i - 1) * w;
-				for (int j = 0; j < w; j++) {
-					int texturePixel = bitmapBuffer[offset1 + j];
-					int blue = (texturePixel >> 16) & 0xff;
-					int red = (texturePixel << 16) & 0x00ff0000;
-					int pixel = (texturePixel & 0xff00ff00) | red | blue;
-					bitmapSource[offset2 + j] = pixel;
-				}
-			}
-		} catch (GLException e) {
-			return null;
-		}
-
-		return Bitmap.createBitmap(bitmapSource, w, h, Bitmap.Config.ARGB_8888);
+	// callback listeners
+	private static void onMediaReceived() {
+		if (sData != 0)
+			onMediaReceived(returnPath, sData);
 	}
+
+	private static void onMediaCanceled() {
+		if (sData != 0)
+			onMediaCanceled(sData);
+	}
+
+	static void onPlaybackComplete() {
+		if (sData != 0)
+			onMediaCompleted(sData);
+	}
+
+	static native void onMediaReceived(String path, long data);
+
+	static native void onMediaCanceled(long data);
+
+	static native void onMediaCompleted(long data);
 
 	//on destroy event
 	public static void onDestroy() {
@@ -306,7 +430,36 @@ class GVideoView extends FrameLayout {
 				FrameLayout.LayoutParams.MATCH_PARENT);
 		params2.gravity = Gravity.CENTER;
 		video.setLayoutParams(params2);
-		video.setVideoPath(path);
+
+//		video.setVideoPath(path);
+//		video.setVideoPath(context.getAssets().open("videos/test.mp4"));
+		// hack copy the gideros video to readable storage
+		File f = new File(context.getCacheDir() + "/" + path);
+		if (!f.exists()) try {
+			String xpath = context.getCacheDir() + "/";
+			InputStream input = context.getAssets().open("assets/" + path);
+			f.getParentFile().mkdirs();
+			int size = input.available();
+			FileOutputStream output = new FileOutputStream(new File(xpath + path));
+			byte data[] = new byte[4096];
+			long total = 0;
+			int count;
+			while ((count = input.read(data)) != -1) {
+				output.write(data, 0, count);
+				total += count;
+				if (size <= total) {
+					break;
+				}
+			}
+			output.flush();
+			output.close();
+			input.close();
+		} catch (Exception e) {
+			Toast.makeText(context, "video not created!", Toast.LENGTH_SHORT).show();
+		}
+
+		Uri uri = Uri.parse(f.getPath());
+		video.setVideoURI(uri);
 		video.requestFocus();
 		video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 			@Override
