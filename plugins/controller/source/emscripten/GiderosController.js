62460488f3c98061dc9211d9262c80a49843943b
Nicolas BOUQUET <hgy01@hieroglyphe.net>
2020-05-18 18:04:15 +0200

    [plugins/controller] Cross browser compatible handler

--- plugins/controller/source/emscripten/GiderosController.js
+++ plugins/controller/source/emscripten/GiderosController.js
@@ -1,12 +1,21 @@
 GidController_getGamepads=function () {
 	var gp=[];
-	navigator.getGamepads().forEach(function (val){
-		var b=[];
-		val.buttons.forEach(function (bt) {
-			b.push({ pressed: bt.pressed, touched: bt.touched, value: bt.value});
-		});
-		var g={ axes: val.axes, id: val.id, index: val.index, mapping: val.mapping, timestamp: val.timestamp, buttons: b};
-		gp.push(g);
-	});
+	var gpi;
+	var gpl=navigator.getGamepads();
+	for (gpi=0;gpi<=gpl.length;gpi++) {
+		var val=gpl[gpi];
+		if (val) {
+			var b=[];
+			var bi;
+			var bl=val.buttons;
+			for (bi=1;bi<=bl.length;bi++) {
+				var bt=bl[bi];
+				if (bt)
+					b.push({ pressed: bt.pressed, touched: bt.touched, value: bt.value});
+			}
+			var g={ axes: val.axes, id: val.id, index: val.index, mapping: val.mapping, timestamp: val.timestamp, buttons: b};
+			gp.push(g);			
+		}
+	}
 	return gp;
 }
