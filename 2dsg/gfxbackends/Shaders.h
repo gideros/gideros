4a0305a77b59b062c1af112bc6730e75bb710491
Nicolas BOUQUET <hgy01@hieroglyphe.net>
2020-11-20 16:22:02 +0100

    [backend/metal] Fix memory leaks

--- 2dsg/gfxbackends/Shaders.h
+++ 2dsg/gfxbackends/Shaders.h
@@ -184,6 +184,28 @@ public:
 		StencilOp dFail;
 		StencilOp dPass;
 		bool sClear;
+        bool operator==(const DepthStencil &o) const {
+            return dTest==o.dTest && dClear==o.dClear;
+        }
+        bool operator<(const DepthStencil &o)  const {
+#define CHECK(v1,v2) if (v1<v2) return true; else if (v1>v2) return false;
+            CHECK(sRef,o.sRef)
+            CHECK(sMask,o.sMask)
+            CHECK(sWMask,o.sWMask)
+            unsigned int d1=(dTest?1:0)|(dClear?2:0)|(sClear?4:0)
+            |(((unsigned int)sFunc)<<4)
+            |(((unsigned int)sFail)<<8)
+            |(((unsigned int)dFail)<<12)
+            |(((unsigned int)dPass)<<16);
+            unsigned int d2=(o.dTest?1:0)|(o.dClear?2:0)|(o.sClear?4:0)
+            |(((unsigned int)o.sFunc)<<4)
+            |(((unsigned int)o.sFail)<<8)
+            |(((unsigned int)o.dFail)<<12)
+            |(((unsigned int)o.dPass)<<16);
+            CHECK(d1,d2)
+#undef CHECK
+            return false;
+        }
 	};
 protected:
 	//CONSTANTS
