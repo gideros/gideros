8c983b06d0d78c1c40771cf1b25f9d444dfc5bb9
MultiPain <32590972+MultiPain@users.noreply.github.com>
2022-02-02 19:06:07 +0900

    Whitespaces format

--- ui/wordhighlighter.h
+++ ui/wordhighlighter.h
@@ -13,32 +13,32 @@
 
 class WordHighlighter : public QObject
 {
-    Q_OBJECT
-    
-    public:
-        WordHighlighter(ScintillaEdit* editor, QSettings& settings);
-        ~WordHighlighter();
-        
-        void setSimpleMode(bool mode);
-        void setEnabled(bool state);
-        void reset();
-        void run();
-    private:
-        ScintillaEdit* editor_;
-        
-        sptr_t cachedPos;
-        sptr_t cachedSelectionStart;
-        sptr_t cachedSelectionEnd;
-        
-        QTimer* updateTimer;
-        bool enabled;
-        bool simpleMode; // detect words ONLY at current cursor position
-        bool filterWord(int style);
-        bool extendBounds(sptr_t& selStart, sptr_t& selEnd);
-        inline void resetCache();
-    private slots:
-        void textAreaClicked(Scintilla::Position line, int modifiers);
-        void timerTimeout();
+	Q_OBJECT
+	
+public:
+	WordHighlighter(ScintillaEdit* editor, QSettings& settings);
+	~WordHighlighter();
+	
+	void setSimpleMode(bool mode);
+	void setEnabled(bool state);
+	void reset();
+	void run();
+private:
+	ScintillaEdit* editor_;
+	
+	sptr_t cachedPos;
+	sptr_t cachedSelectionStart;
+	sptr_t cachedSelectionEnd;
+	
+	QTimer* updateTimer;
+	bool enabled;
+	bool simpleMode; // detect words ONLY at current cursor position
+	bool filterWord(int style);
+	bool extendBounds(sptr_t& selStart, sptr_t& selEnd);
+	inline void resetCache();
+private slots:
+	void textAreaClicked(Scintilla::Position line, int modifiers);
+	void timerTimeout();
 };
 
 #endif // WORDHIGHLIGHTER_H
