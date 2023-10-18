db7bd4345778d0da72b3c8cf6111abb63068540b
hgy29 <hgy01@hieroglyphe.net>
2023-10-18 17:33:50 +0200

    [gfx/metal] Recreate texture on update if necessary

--- 2dsg/gfxbackends/metal/metalShaderTexture.mm
+++ 2dsg/gfxbackends/metal/metalShaderTexture.mm
@@ -84,46 +84,60 @@ metalShaderTexture::metalShaderTexture(ShaderTexture::Format format,ShaderTextur
 
 void metalShaderTexture::updateData(ShaderTexture::Format format,ShaderTexture::Packing packing,int width,int height,const void *data,ShaderTexture::Wrap wrap,ShaderTexture::Filtering filtering)
 {
+	if ((width!=this->width)||(height!=this->height)||(format!=this->format))
+	{
+	    MTLPixelFormat glformat=MTLPixelFormatRGBA8Unorm;
+	    bpr=4;
+	    switch (packing)
+	    {
+	    	case PK_UBYTE: {
+	    	    switch (format)
+	    	    {
+	    	    	case FMT_ALPHA: glformat=MTLPixelFormatA8Unorm; bpr=1; break;
+	    	    	case FMT_RGB: glformat=MTLPixelFormatRGBA8Unorm; break;
+	    	    	case FMT_RGBA: glformat=MTLPixelFormatRGBA8Unorm; break;
+	    	    	case FMT_Y: glformat=MTLPixelFormatR8Unorm; bpr=1; break;
+	    	    	case FMT_YA: glformat=MTLPixelFormatRG8Unorm; bpr=2; break;
+	    	    }
+	    	}/* not handled on OSX
+	    	case PK_USHORT_565: {
+	    	    switch (format)
+	    	    {
+	    	    	case FMT_RGB: glformat=MTLPixelFormatB5G6R5Unorm; bpr=2; break;
+	    	    }
+	    		break;
+	    	}
+	    	case PK_USHORT_4444:  
+	    	    switch (format)
+	    	    {
+	    	    	case FMT_RGBA: glformat=MTLPixelFormatABGR4Unorm; bpr=2; break;
+	    	    }
+	    		break;
+	    	case PK_USHORT_5551: 
+	    	    switch (format)
+	    	    {
+	    	    	case FMT_RGBA: glformat=MTLPixelFormatA1BGR5Unorm; bpr=2; break;
+	    	    }
+	    		break;*/
+	    }
+		MTLTextureDescriptor * md=[MTLTextureDescriptor texture2DDescriptorWithPixelFormat:(MTLPixelFormat)glformat 
+		                                                       width:(NSUInteger)width 
+		                                                      height:(NSUInteger)height 
+		                                                   mipmapped:(BOOL)NO];
+        md.usage=[mtex usage];
+	    if (format==FMT_DEPTH) {
+	        md.storageMode=MTLStorageModePrivate; //Depth can only be private as per spec
+	        data=NULL; //Don't try to upload data
+	    }
+        [mtex release];
+		mtex=[metalDevice newTextureWithDescriptor:md];
+	}
 	this->width=width;
 	this->height=height;
 	this->wrap=wrap;
 	this->filter=filtering;
     this->format=format;
 
-    MTLPixelFormat glformat=MTLPixelFormatRGBA8Unorm;
-    bpr=4;
-    switch (packing)
-    {
-    	case PK_UBYTE: {
-    	    switch (format)
-    	    {
-    	    	case FMT_ALPHA: glformat=MTLPixelFormatA8Unorm; bpr=1; break;
-    	    	case FMT_RGB: glformat=MTLPixelFormatRGBA8Unorm; break;
-    	    	case FMT_RGBA: glformat=MTLPixelFormatRGBA8Unorm; break;
-    	    	case FMT_Y: glformat=MTLPixelFormatR8Unorm; bpr=1; break;
-    	    	case FMT_YA: glformat=MTLPixelFormatRG8Unorm; bpr=2; break;
-    	    }
-    	}/* not handled on OSX
-    	case PK_USHORT_565: {
-    	    switch (format)
-    	    {
-    	    	case FMT_RGB: glformat=MTLPixelFormatB5G6R5Unorm; bpr=2; break;
-    	    }
-    		break;
-    	}
-    	case PK_USHORT_4444:  
-    	    switch (format)
-    	    {
-    	    	case FMT_RGBA: glformat=MTLPixelFormatABGR4Unorm; bpr=2; break;
-    	    }
-    		break;
-    	case PK_USHORT_5551: 
-    	    switch (format)
-    	    {
-    	    	case FMT_RGBA: glformat=MTLPixelFormatA1BGR5Unorm; bpr=2; break;
-    	    }
-    		break;*/
-    }
     if (data) {
     	[mtex replaceRegion:MTLRegionMake2D(0,0,width,height) 
          mipmapLevel:0 
